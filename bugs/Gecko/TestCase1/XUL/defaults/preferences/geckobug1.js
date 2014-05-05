e409a3d7fbe7b1012ba6308b60b525e2b1364757
csimon <epeios-q37@users.noreply.github.com>
2014-05-05 18:52:52 +0200

    05/05/2014 18:52:52

---
+++ bugs/Gecko/TestCase1/XUL/defaults/preferences/geckobug1.js
@@ -0,0 +1,8 @@
+// $Id: prefs.js,v 1.4 2009/10/09 15:46:11 csimon Exp $
+pref("toolkit.defaultChromeURI", "chrome://geckobug1/content/Main.xul");
+pref("browser.formfill.enable", true );
+pref("UniversalBrowserWrite", true );
+
+// Following both lines permit to change the locale.
+pref("intl.locale.matchOS", "false");
+pref("general.useragent.locale", "en");
