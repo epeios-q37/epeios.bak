266cf639f7feacb48c59ee6a31fafcc537b1527e
csimon <epeios-q37@users.noreply.github.com>
2014-07-09 13:41:02 +0200

    09/07/2014 13:41:01

--- stable/sck.h
+++ stable/sck.h
@@ -121,7 +121,7 @@ extern class ttr_tutor &SCKTutor;
 #endif
 
 //d Max data amount of concurrent write and read.
-#define SCK__DEFAULT_AMOUNT	( 1024 * 1024 )
+#define SCK__DEFAULT_AMOUNT	( 100 * 1024 * 1024 )
 
 namespace sck {
 	using flw::datum__;
