d89552fef7ee40cc4f59fca4c157e181066af05a
csimon <epeios-q37@users.noreply.github.com>
2015-05-31 12:21:25 +0200

    31/05/2015 12:21:23

--- tools/xppq/registry.h
+++ tools/xppq/registry.h
@@ -1,20 +1,20 @@
 /*
-	Copyright (C) 2007, 2008, 2010-2015 Claude SIMON (http://q37.info/contact/).
+	Copyright (C) 2007-2015 Claude SIMON (http://q37.info/contact/).
 
-	This file is part of the xppq tool.
+	This file is part of xppq.
 
-	The xppq tool is free software: you can redistribute it and/or
+	xppq is free software: you can redistribute it and/or
 	modify it under the terms of the GNU Affero General Public License as
 	published by the Free Software Foundation, either version 3 of the
 	License, or (at your option) any later version.
 
-	The xppq tool is distributed in the hope that it will be useful,
+	xppq is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 	Affero General Public License for more details.
 
 	You should have received a copy of the GNU Affero General Public License
-	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
+	along with xppq. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef REGISTRY__INC
