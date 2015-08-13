6ca20715008fbc239ca97bb37a074fa46608cda3
csimon <epeios-q37@users.noreply.github.com>
2015-08-13 08:35:42 +0200

    13/08/2015 08:35:42

---
+++ stable/strmrg.cpp
@@ -0,0 +1,465 @@
+/*
+	Copyright (C) 2000-2015 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of the Epeios framework.
+
+	The Epeios framework is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	The Epeios framework is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#define STRMRG__COMPILATION
+
+#include "strmrg.h"
+
+/******************************************************************************/
+				  /* do not modify anything above this limit */
+				  /*			  unless specified			 */
+				  /*******************************************/
+
+#include "flx.h"
+
+using namespace strmrg;
+
+row__ strmrg::table_::Append( const str::string_ &String )
+{
+	row__ Row = qNIL;
+qRH
+	_item Item;
+qRB
+	Item.Init( Strings.Append( String ) );
+
+	Row = Main.Append( Items.Append( Item ) );
+qRR
+qRT
+qRE
+	return Row;
+}
+
+// Prdclaration.
+static void Append_(
+	const _irows_ &SourceRows,
+	const _items_ &SourceItems,
+	const _strings_ &SourceStrings,
+	_irows_ &TargetRows,
+	_items_ &TargetItems,
+	_strings_ &TargetStrings );
+
+static _irow__ Append_(
+	const _item_ &SourceItem,
+	const _items_ &SourceItems,
+	const _strings_ &SourceStrings,
+	_items_ &TargetItems,
+	_strings_ &TargetStrings )
+{
+	_irow__ Row = qNIL;
+qRH
+	_item Item;
+	ctn::E_CMITEMt( _string_ , _srow__ ) SourceString;
+qRB
+	if ( SourceItem.ContainsString() ) {	// Positionne 'SourceItem()' pour la suite !
+		SourceString.Init( SourceStrings );
+		Item.Init( TargetStrings.Append( SourceString( SourceItem.String() ) ) );
+	} else {
+		Item.Init();
+		Append_( SourceItem.Items, SourceItems, SourceStrings, Item.Items, TargetItems, TargetStrings );
+	}
+
+	Row = TargetItems.Append( Item ) ;
+qRR
+qRT
+qRE
+	return Row;
+}
+
+static _irow__ Append_(
+	_irow__ SourceRow,
+	const _items_ &SourceItems,
+	const _strings_ &SourceStrings,
+	_items_ &TargetItems,
+	_strings_ &TargetStrings )
+{
+	ctn::E_CMITEMt( _item_, _irow__ ) SourceItem;
+
+	SourceItem.Init( SourceItems );
+
+	return Append_( SourceItem( SourceRow ), SourceItems, SourceStrings, TargetItems, TargetStrings );
+}
+
+static void Append_(
+	const _irows_ &SourceRows,
+	const _items_ &SourceItems,
+	const _strings_ &SourceStrings,
+	_irows_ &TargetRows,
+	_items_ &TargetItems,
+	_strings_ &TargetStrings )	// Rcursif (paaaaas bien) !
+{
+	ctn::E_CMITEMt( _item_, _irow__ ) SourceItem;
+	row__ Row = SourceRows.First();
+
+	SourceItem.Init( SourceItems );
+
+	while ( Row != qNIL ) {
+		TargetRows.Append( Append_( SourceRows( Row ), SourceItems, SourceStrings, TargetItems, TargetStrings ) );
+
+		Row = SourceRows.Next( Row );
+	}
+}
+
+row__ strmrg::table_::AppendMono( const table_ &Table )
+{
+	row__ Row = qNIL;
+qRH
+	_item Item;
+	ctn::E_CMITEMt( _item_, _irow__ ) SourceItem;
+	ctn::E_CMITEMt( _string_, _srow__ ) SourceString;
+qRB
+	SourceItem.Init( Table.Items );
+
+	if ( (Table.Amount() == 1) && (SourceItem(Table.Main(Table.Main.First())).ContainsString()) ) {
+		SourceString.Init( Table.Strings );
+		Row = Append( SourceString( SourceItem().String() ) );
+	} else {
+		Item.Init();
+
+		Append_( Table.Main, Table.Items, Table.Strings, Item.Items, Items, Strings );
+
+		Row = Main.Append( Items.Append( Item ) );
+	}
+qRR
+qRT
+qRE
+	return Row;
+}
+
+void strmrg::table_::AppendMulti( const table_ &Table )
+{
+qRH
+	retriever__ Retriever;
+	table SubTable;
+qRB
+	Retriever.Init( Table );
+
+	while ( Retriever.Availability() != aNone ) {
+		SubTable.Init();
+		Retriever.GetTable( SubTable );
+		AppendMono( SubTable );
+	}
+qRR
+qRT
+qRE
+}
+
+row__ strmrg::table_::AppendMono( const str::strings_ &Strings )
+{
+	row__ Row = qNIL;
+qRH
+	table Table;
+	ctn::E_CMITEM( str::string_ ) String;
+	sdr::row__ SRow = qNIL;
+qRB
+	Table.Init();
+	SRow = Strings.First();
+
+	String.Init( Strings );
+
+	while ( SRow != qNIL ) {
+		Table.Append( String( SRow ) );
+
+		SRow = Strings.Next( SRow );
+	}
+
+	Row = AppendMono( Table ); 
+qRR
+qRT
+qRE
+	return Row;
+}
+
+void strmrg::table_::AppendMulti( const str::strings_ &Strings )
+{
+qRH
+	ctn::E_CMITEM( str::string_ ) String;
+	sdr::row__ Row = Strings.First();
+qRB
+	Row = Strings.First();
+
+	String.Init( Strings );
+
+	while ( Row != qNIL ) {
+		Append( String( Row ) );
+
+		Row = Strings.Next( Row );
+	}
+qRR
+qRT
+qRE
+}
+
+void strmrg::table_::GetTable(
+	row__ Row,
+	table_ &Table ) const
+{
+	ctn::E_CMITEMt( _item_, _irow__ ) Item;
+	Item.Init( Items );
+
+	Append_( Item( Main( Row ) ).Items, Items, Strings, Table.Main, Table.Items, Table.Strings );
+}
+
+void strmrg::GetTable(
+	const _item_ &Item,
+	const _items_ &Items,
+	const _strings_ &Strings,
+	table_ &Table )
+{
+	ctn::E_CMITEMt( _string_, _srow__ ) String;
+
+	if ( Item.ContainsString() ) {
+		String.Init( Strings );
+		Table.Append( String( Item.String() ) );
+	} else
+		Append_( Item.Items, Items, Strings, Table.Main, Table.Items, Table.Strings );
+}
+
+static void Put_(
+	const str::string_ &String,
+	const tokens__ &Tokens,
+	flw::oflow__ &Flow )
+{
+	const char Pattern[5] = { Tokens.Separator, Tokens.Begin, Tokens.End, Tokens.Escape, 0 };
+	sdr::row__ Row = String.First();
+	bso::char__ Char = 0;
+
+	while ( Row != qNIL ) {
+		if ( strrchr( Pattern, Char = String( Row ) ) )
+			Flow << Tokens.Escape;
+
+		Flow << Char;
+
+		Row = String.Next( Row );
+	}
+}
+
+static void Merge_(
+	const _irows_ &Rows,
+	const _items_ &Items,
+	const _strings_ &Strings,
+	const tokens__ &Tokens,
+	flw::oflow__ &Flow )	// Rcursif (paaas bien !) !
+{
+	ctn::E_CMITEMt( _item_, _irow__ ) Item;
+	ctn::E_CMITEMt( _string_, _srow__ ) String;
+
+	row__ Row = Rows.First();
+	Item.Init( Items );
+	String.Init( Strings );
+
+	while ( Row != qNIL ) {
+		if ( Item( Rows ( Row ) ).ContainsString() ) {	// Positionne 'Item' pour la suite !
+
+			Put_( String( Item().String() ), Tokens, Flow );
+
+			Flow << Tokens.Separator;
+		} else {
+			Flow << Tokens.Begin;
+			Merge_( Item().Items, Items, Strings, Tokens, Flow );
+			Flow << Tokens.End;
+		}
+
+		Row = Rows.Next( Row );
+	}
+}
+
+void strmrg::Merge(
+	const table_ &Table,
+	flw::oflow__ &Flow,
+	const tokens__ &Tokens )
+{
+	Merge_( Table.Main, Table.Items, Table.Strings, Tokens, Flow );
+}
+
+void strmrg::Merge(
+	const table_ &Table,
+	str::string_ &String,
+	const tokens__ &Tokens )
+{
+qRH
+	flx::E_STRING_OFLOW___ Flow;
+qRB
+	Flow.Init( String );
+
+	Merge( Table, Flow, Tokens );
+qRR
+qRT
+qRE
+}
+
+enum trace__ {
+	tOther,
+	tTable,
+	tEscape,
+	t_amount,
+	t_Undefined
+};
+
+bso::bool__ strmrg::Split(
+	xtf::extended_text_iflow__ &Flow,
+	table_ &Table,
+	const tokens__ &Tokens )	// Rcursif !
+{
+	bso::bool__ Success = true;
+qRH
+	str::string String;
+	table SubTable;
+	trace__ Trace = t_Undefined;
+	xtf::utf__ UTF;
+	bso::char__ C = 0;
+	xtf::error__ Error = xtf::e_Undefined;
+qRB
+	String.Init();
+
+	while ( !Flow.EndOfFlow( Error ) ) {
+		UTF.Init();
+		C = Flow.Get( UTF );
+		if ( Trace == tEscape ) {
+			String.Append( (bso::char__ *)UTF.Data, UTF.Size );
+			Trace = tOther;
+		} else {
+			if ( C == Tokens.Escape ) {
+				Trace = tEscape;
+			} else if ( C == Tokens.Separator ) {
+				if ( ( String.Amount() != 0 ) || ( Trace != tTable ) )
+					Table.Append( String );
+				Trace = tOther;
+				String.Init();
+			} else if ( C == Tokens.Begin ) {
+				if ( String.Amount() != 0 ) {
+					Table.Append( String );
+					String.Init();
+				}
+
+				SubTable.Init();
+				if ( !Split( Flow, SubTable, Tokens ) ) {
+					Success = false;
+					qRFwk();
+				}
+				Table.AppendMono( SubTable );
+				Trace = tTable;
+			} else if ( C == Tokens.End ) {
+				if ( String.Amount() != 0 )
+					Table.Append( String );
+				qRReturn;
+			} else {
+				String.Append( (bso::char__ *)UTF.Data, UTF.Size );
+				Trace = tOther;
+			}
+		}
+	}
+
+	if ( ( Error == xtf::e_NoError ) && ( Trace != tEscape ) ) {
+		if ( String.Amount() != 0 )
+			Table.Append( String );
+	} else
+		Success = false;
+
+qRR
+qRT
+qRE
+	return Success;
+}
+
+bso::bool__ strmrg::Split(
+	const str::string_ &String,
+	table_ &Table,
+	const tokens__ &Tokens )
+{
+	flx::E_STRING_IFLOW__ SFlow;
+	xtf::extended_text_iflow__ XFlow;
+
+	SFlow.Init( String );
+	XFlow.Init( SFlow, utf::f_Guess );
+
+	return Split( XFlow, Table, Tokens );
+}
+
+void strmrg::retriever__::GetString( str::string_ &String )
+{
+qRH
+	str::strings Strings;
+qRB
+	Strings.Init();
+
+	GetStrings( Strings );
+
+	if ( Strings.Amount() != 1 )
+		qRFwk();
+
+	Strings.Recall(Strings.First(), String );
+qRR
+qRT
+qRE
+}
+
+static void GetStrings_(
+	const _irows_ &Rows,
+	const _items_ &Items,
+	const _strings_ &Strings,
+	str::strings_ &Result )
+{
+	ctn::E_CMITEMt( _item_, _irow__ ) Item;
+	ctn::E_CMITEMt( _string_, _srow__ ) String;
+	row__ Row = Rows.First();
+
+	Item.Init( Items );
+	String.Init( Strings );
+
+	while ( Row != qNIL ) {
+		if ( !Item(Rows( Row ) ).ContainsString() )
+			qRFwk();
+
+		Result.Append( String( Item().String() ) );
+
+		Row = Rows.Next( Row );
+	}
+}
+
+static void GetStrings_(
+	const _item_ &Item,
+	const _items_ &Items,
+	const _strings_ &Strings,
+	str::strings_ &Result )
+{
+	ctn::E_CMITEMt( _string_, _srow__ ) String;
+	String.Init( Strings );
+
+	if ( Item.ContainsString() )
+		Result.Append( String( Item.String() ) );
+	else {
+		GetStrings_( Item.Items, Items, Strings, Result );
+	}
+}
+
+void strmrg::retriever__::GetStrings( str::strings_ &Result )
+{
+	ctn::E_CMITEMt( _item_, _irow__ ) Item;
+	ctn::E_CMITEMt( _string_, _srow__ ) String;
+
+	Item.Init( _I() );
+	String.Init( s_() );
+
+	if ( Item( _R()( _Row ) ).ContainsString() )	// Positionne 'Item()'.
+		Result.Append( String( Item().String() ) );
+	else
+		GetStrings_( Item(_R()( _Row ) ), _I(), s_(), Result );
+
+	_Row = _R().Next( _Row );
+}
---
+++ stable/strmrg.h
@@ -0,0 +1,368 @@
+/*
+	Copyright (C) 2000-2015 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of the Epeios framework.
+
+	The Epeios framework is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	The Epeios framework is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef STRMRG__INC
+# define STRMRG__INC
+
+# define STRMRG_NAME		"STRMRG"
+
+# if defined( E_DEBUG ) && !defined( STRMRG_NODBG )
+#  define STRMRG_DBG
+# endif
+
+/******************************************************************************/
+				  /* do not modify anything above this limit */
+				  /*			  unless specified			 */
+				  /*******************************************/
+
+// STRings MeRGer
+
+# include "err.h"
+# include "flw.h"
+
+# include "err.h"
+# include "flw.h"
+
+# include "ctn.h"
+# include "str.h"
+# include "xtf.h"
+
+
+namespace strmrg { 
+	qROW( row__ );
+
+	qROW( _irow__ );	// Item row.
+
+# if 0	// Sinon VC++ perd les pdales lors de l'dition de liens...
+	namespace {
+# endif
+		qROW( _srow__);
+		typedef str::string_ _string_;
+		typedef ctn::E_MCONTAINERt_( _string_, _srow__ ) _strings_;
+		E_AUTO( _strings );
+
+		typedef bch::E_BUNCHt_( _irow__, row__ ) _irows_;
+		E_AUTO( _irows );
+
+		typedef _irows_ _table_;
+		E_AUTO( _table );
+
+		class _item_
+		{
+		public:
+			struct s {
+				_srow__ String;
+				_irows_::s Items;
+			} &S_;
+			_irows_ Items;
+			_item_( s &S )
+			: S_( S ),
+			  Items( S.Items )
+			{}
+			void reset( bso::bool__ P = true )
+			{
+				S_.String = qNIL;
+				Items.reset( P );
+			}
+			void plug( qSD__ &SD )
+			{
+				Items.plug( SD );
+			}
+			void plug( qAS_ &AS )
+			{
+				Items.plug( AS );
+			}
+			_item_ &operator =( const _item_ &I )
+			{
+				S_.String = I.S_.String;
+				Items = I.Items;
+
+				return *this;
+			}
+			void Init( _srow__ String = qNIL )
+			{
+				if ( ( S_.String != qNIL ) || ( Items.Amount() != 0 ) )
+					qRFwk();
+
+				S_.String = String;
+				Items.Init();
+			}
+			E_RODISCLOSE_( _srow__, String );
+			void Append( _irow__ Item )
+			{
+				if ( S_.String != qNIL )
+					qRFwk();
+
+				Items.Append( Item );
+			}
+			bso::bool__ ContainsItems( void ) const
+			{
+				return Items.Amount() != 0;
+			}
+			bso::bool__ ContainsString( void ) const
+			{
+				return S_.String != qNIL;
+			}
+		};
+
+		E_AUTO( _item );
+
+		typedef ctn::E_MCONTAINERt_( _item_, _irow__ ) _items_;
+		E_AUTO( _items );
+# if 0
+	}
+# endif
+	class table_
+	{
+	public:
+		struct s {
+			_strings_::s Strings;
+			_items_::s Items;
+			_table_::s Main;
+		};
+		_strings_ Strings;
+		_items_ Items;
+		_table_ Main;
+		table_( s &S )
+		: Strings( S.Strings ),
+		  Items( S.Items ),
+		  Main( S.Main )
+		{}
+		void reset( bso::bool__ P = true )
+		{
+			Strings.reset( P );
+			Items.reset( P );
+			Main.reset( P );
+		}
+		void plug( qAS_ &AS )
+		{
+			Strings.plug( AS );
+			Items.plug( AS );
+			Main.plug( AS );
+		}
+		table_ &operator =(const table_ &T)
+		{
+			Strings = T.Strings;
+			Items = T.Items;
+			Main = T.Main;
+
+			return *this; 
+		}
+		void Init( void )
+		{
+			Strings.Init();
+			Items.Init();
+			Main.Init();
+		}
+		row__ Append( const str::string_ &String );
+		row__ AppendMono( const table_ &Table );	// 'Table' is stored in one entry.
+		void AppendMulti( const table_ &Table );	// An entry is created for each (first-level) entry of 'Table' 
+		row__ AppendMono( const str::strings_ &Strings );	// ATTENTION : Cre une seule entre avec tous les strings, et pas autant d'entre que de string.
+		void AppendMulti( const str::strings_ &Strings );	// ATTENTION : Ajoute autant d'entre que de 'Strings'.
+		void GetTable(
+			row__ Row, 
+			table_ &Table ) const;
+		E_NAVt( Main., row__ );
+	};
+
+	void GetTable(
+		const _item_ &Item,
+		const _items_ &Items,
+		const _strings_ &Strings,
+		table_ &Table );
+
+	E_CDEF(char, DefaultSeparatorToken, '|');
+	E_CDEF(char, DefaultBeginToken, '(');
+	E_CDEF(char, DefaultEndToken, ')');
+	E_CDEF(char, DefaultEscapeToken, '\\');
+
+	struct tokens__ {
+		char
+			Separator,	// Caractre sparant les diffrentes entres.
+			Begin,		// Caractre marquant le dbut d'un groupe.
+			End,		// Caractre marquant la fin d'un groupe.
+			Escape;		// Caratre d'chappement.
+		void reset( bso::bool__ = true )
+		{
+			Separator = DefaultSeparatorToken;
+			Begin = DefaultBeginToken;
+			End = DefaultEndToken,
+			Escape = DefaultEscapeToken;
+		}
+		E_CDTOR( tokens__ );
+		tokens__( char Separator )
+		{
+			reset( false );
+
+			Init( Separator );
+		}
+		tokens__(
+			char Separator,
+			char Begin, 
+			char End,
+			char Escape = DefaultEscapeToken )
+		{
+			reset( false );
+
+			Init( Separator, Begin, End, Escape );
+		}
+		void Init(
+			char Separator,
+			char Begin,
+			char End,
+			char Escape = DefaultEscapeToken )
+		{
+			this->Separator = Separator;
+			this->Begin = Begin;
+			this->End = End;
+			this->Escape = Escape;
+		}
+		void Init( char Separator )
+		{
+			Init( Separator, DefaultBeginToken, DefaultEndToken );
+		}
+	};
+
+	E_AUTO( table );
+
+	void Merge(
+		const table_ &Table,
+		flw::oflow__ &Flow,
+		const tokens__ &Tokens = tokens__() );
+
+	void Merge(
+		const table_ &Table,
+		str::string_ &String,
+		const tokens__ &Tokens = tokens__() );
+
+	bso::bool__ Split(
+		xtf::extended_text_iflow__ &Flow,
+		table_ &Table,
+		const tokens__ &Tokens = tokens__() );
+
+	bso::bool__ Split(
+		const str::string_ &String,
+		table_ &Table,
+		const tokens__ &Tokens = tokens__() );
+
+	enum availability__ {
+		aNone,		// Pas d'autres paramtres disponible.
+		aEmpty,		// Le prochain paramtre est vide.
+		aString,	// Le prochain paramtre est un 'string'.
+		aTable,		// Le prochain paramtre est une table.
+		a_amount,
+		a_Undefined
+	};
+
+	class retriever__ {
+	private:
+		const _strings_ *_Strings;
+		const _items_ *_Items;
+		const _irows_ *_Rows;
+		row__ _Row;
+		const _strings_ &s_( void ) const
+		{
+			if ( _Strings == NULL )
+				qRFwk();
+
+			return *_Strings;
+		}
+		const _items_ &_I( void ) const
+		{
+			if ( _Items == NULL )
+				qRFwk();
+
+			return *_Items;
+		}
+		const _irows_ &_R( void ) const
+		{
+			if ( _Rows == NULL )
+				qRFwk();
+
+			return *_Rows;
+		}
+	public:
+		void reset( bso::bool__ = true )
+		{
+			_Strings = NULL;
+			_Items = NULL;
+			_Rows = NULL;
+			_Row = qNIL;
+		}
+		E_CDTOR( retriever__ );
+		void Init(
+			const _strings_ &Strings,
+			const _items_ &Items,
+			const _irows_ &Rows )
+		{
+			_Strings = &Strings;
+			_Items = &Items;
+			_Rows = &Rows;
+
+			_Row = Rows.First();
+		}
+		void Init( const table_ &Table )
+		{
+			Init( Table.Strings, Table.Items, Table.Main );
+		}
+		void GetString( str::string_ &Target );
+		void GetTable( table_ &Table )
+		{
+			ctn::E_CMITEMt( _item_, _irow__ ) Item;
+			Item.Init( _I() );
+
+			strmrg::GetTable( Item( _R()( _Row ) ), _I(), s_(), Table );
+
+			_Row = _R().Next( _Row );
+		}
+		void Skip( void )
+		{
+			_Row = _R().Next( _Row );
+		}
+		// Ne retourne que le contenu d'une seule entre. L'entre en cours doit tre un 'string', ou une table ne contenant que des 'string's, sinon erreur.
+		void GetStrings( str::strings_ &Strings );
+		availability__ Availability( void ) const
+		{
+			availability__ Availability = a_Undefined;
+
+			if ( _Row == qNIL )
+				Availability = aNone;
+			else {
+				ctn::E_CMITEMt( _item_, _irow__ ) Item;
+				Item.Init( _I() );
+
+				if ( Item( _R()( _Row ) ).ContainsString() )	// Positionne 'Item()'.
+					Availability = aString;
+				else if ( Item().ContainsItems() )
+					Availability = aTable;
+				else
+					Availability = aEmpty;
+			}
+
+			return Availability;
+		}
+	};
+}
+
+				  /********************************************/
+				  /* do not modify anything belove this limit */
+				  /*			  unless specified		   	  */
+/******************************************************************************/
+
+#endif
