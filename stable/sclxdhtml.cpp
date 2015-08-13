9754313c877a0fed39d25f0da12f245be923440d
csimon <epeios-q37@users.noreply.github.com>
2015-08-13 09:55:54 +0200

    13/08/2015 09:55:54

---
+++ stable/sclxdhtml.cpp
@@ -0,0 +1,603 @@
+/*
+	Copyright (C) 2000-2015 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of the Epeios framework.
+
+	The Epeios framework is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	The Epeios framework is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#define SCLXDHTML__COMPILATION
+
+#include "sclxdhtml.h"
+
+#include "sclfrntnd.h"
+
+using namespace sclxdhtml;
+
+static bso::bool__ IsInitialized_ = false;
+
+static const char *Launcher_ = NULL;
+
+const sclrgstry::registry_ &sclxdhtml::GetRegistry( void )
+{
+	return sclrgstry::GetCommonRegistry();
+}
+
+const char *sclxdhtml::GetLauncher( void )
+{
+	if ( Launcher_ == NULL )
+		qRFwk();
+
+	return Launcher_;
+}
+
+#ifdef CPE_S_WIN
+# define FUNCTION_SPEC __declspec(dllexport)
+#else
+#define FUNCTION_SPEC
+# endif
+
+// Bien que dfinit dans un '.cpp', et propre  ce '.cpp', VC++ se mlange les pinceaux avec le 'callback__' dfinit dans 'scllocale.cpp', d'o le 'namespace'.
+namespace {
+
+	typedef xdhcbk::downstream_callback__ _downstream_callback__;
+
+	class donwstream_callback_implementation__
+	: public _downstream_callback__
+	{
+	protected:
+		virtual void XDHCBKInitialize( const xdhcbk::shared_data__ &Data ) override
+		{
+			if ( Launcher_ != NULL )
+				qRFwk();
+
+			if ( Launcher_ == NULL ) {
+				Launcher_ = Data.LauncherIdentification();
+				sclmisc::Initialize( Data.qRRor(), Data.SCLError(), Data.CIO(), Data.Localization() );
+				sclfrntnd::LoadProject();
+
+				if ( Data.Mode() == frdbse::mMultiUser )
+					sclfrntnd::Connect();
+
+				SCLXDHTMLInitialization( Data.Mode() );
+			}
+		}
+		virtual void XDHCBKBaseLanguage( TOL_CBUFFER___ &Buffer ) override
+		{
+			const char *Language = sclmisc::GetBaseLanguage();
+
+			if ( Language == NULL )
+				qRFwk();
+
+			Buffer.Malloc(strlen( Language) + 1 );
+
+			strcpy( Buffer, Language );
+		}
+		virtual xdhcbk::session_callback__ *XDHCBKNew(
+			const char *Language,
+			xdhcbk::proxy_callback__ *ProxyCallback ) override
+		{
+			return SCLXDHTMLNew( Language, ProxyCallback );
+		}
+	public:
+		void reset( bso::bool__ P = true )
+		{
+			downstream_callback__::reset( P );
+		}
+		E_CVDTOR( donwstream_callback_implementation__ );
+		void Init( void )
+		{
+			downstream_callback__::Init();
+		}
+	};
+}
+
+static inline void DoNothing_( void )
+{}
+
+#define DEF( name, function ) extern "C" FUNCTION_SPEC function name
+
+DEF( XDHCBK_RETRIEVE_FUNCTION_NAME, xdhcbk::retrieve );
+
+xdhcbk::downstream_callback__ *XDHCBKRetrieve( void )
+{
+	donwstream_callback_implementation__ *Callback = NULL;
+qRFH
+qRFB
+	Callback = new donwstream_callback_implementation__;
+
+	if ( Callback == NULL )
+		qRAlc();
+
+	Callback->Init();
+qRFR
+	if ( Callback != NULL )
+		delete Callback;
+
+	Callback = NULL;
+qRFT
+qRFE(DoNothing_())
+	return Callback;
+}
+
+namespace {
+	void SetXML_(
+		const str::string_  &Message,
+		str::string_ &XML )
+	{
+	qRH
+		flx::E_STRING_TOFLOW___ STOFlow;
+		xml::writer Writer;
+	qRB
+		STOFlow.Init( XML );
+		Writer.Init( STOFlow, xml::oCompact, xml::e_Default );
+
+		Writer.PutValue( Message, "Content" );
+	qRR
+	qRT
+	qRE
+	}
+
+	void SetXML_(
+		const str::string_  &RawMessage,
+		const char *Language,
+		str::string_ &XML )
+	{
+	qRH
+		str::string Message;
+		TOL_CBUFFER___ Buffer;
+	qRB
+		Message.Init();
+		scllocale::GetTranslation( RawMessage.Convert( Buffer ), Language, Message );
+
+		SetXML_( Message, XML );
+	qRR
+	qRT
+	qRE
+	}
+
+	inline void SetXSL_( str::string_ &XSL )
+	{
+		XSL.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\
+			<xsl:stylesheet version=\"1.0\"\
+			                xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">\
+				<xsl:output method=\"text\"\
+					        encoding=\"utf-8\"/>\
+				<xsl:template match=\"/\">\
+					<xsl:value-of select=\"Content\"/>\
+				</xsl:template>\
+			</xsl:stylesheet>\
+		");
+	}
+
+	inline void SetXMLAndXSL_(
+		const ntvstr::string___ &Message,
+		const char *Language,
+		str::string_ &XML,
+		str::string_ &XSL )
+	{
+	qRH
+		str::string Buffer;
+	qRB
+		Buffer.Init();
+
+		if ( Language == NULL )
+			SetXML_( Message.UTF8( Buffer ), XML );
+		else
+			SetXML_( Message.UTF8( Buffer ), Language, XML );
+
+		SetXSL_( XSL );
+	qRR
+	qRT
+	qRE
+	}
+}
+
+void sclxdhtml::Alert(
+	const ntvstr::string___ &XML,
+	const ntvstr::string___ &XSL,
+	const ntvstr::string___ &Title,
+	proxy__ &Proxy,
+	const char *Language )
+{
+qRH
+	str::string CloseText;
+qRB
+	CloseText.Init();
+	scllocale::GetTranslation( SCLXDHTML_NAME "_CloseText", Language, CloseText );
+
+	Proxy.Alert( XML, XSL, Title, CloseText );
+qRR
+qRT
+qRE
+}
+
+namespace{
+	void Alert_(
+		const ntvstr::string___ &Message,
+		const char *MessageLanguage,	// If != 'NULL', 'Message' is translates, otherwise it is displayed as is.
+		proxy__ &Proxy,
+		const char *CloseTextLanguage )
+	{
+	qRH
+		str::string XML, XSL;
+	qRB
+		XML.Init();
+		XSL.Init();
+
+		SetXMLAndXSL_( Message, MessageLanguage, XML, XSL );
+
+		Alert(XML, XSL, ntvstr::string___(), Proxy, CloseTextLanguage );
+	qRR
+	qRT
+	qRE
+	}
+}
+
+void sclxdhtml::Alert(
+	const ntvstr::string___ &RawMessage,
+	const char *Language,
+	proxy__ &Proxy )
+{
+	Alert_( RawMessage, Language, Proxy, Language );
+}
+
+void sclxdhtml::Alert(
+	const ntvstr::string___ &Message,
+	proxy__ &Proxy,
+	const char *Language )
+{
+	Alert_( Message, NULL, Proxy, Language );
+}
+
+bso::bool__ sclxdhtml::Confirm(
+	const ntvstr::string___ &XML,
+	const ntvstr::string___ &XSL,
+	const ntvstr::string___ &Title,
+	proxy__ &Proxy,
+	const char *Language )
+{
+	bso::bool__ Confirmation = false;
+qRH
+	str::string CloseText;
+qRB
+	CloseText.Init();
+	scllocale::GetTranslation( SCLXDHTML_NAME "_CloseText", Language, CloseText );
+
+	Confirmation = Proxy.Confirm( XML, XSL, Title, CloseText );
+qRR
+qRT
+qRE
+	return Confirmation;
+}
+
+namespace {
+	bso::bool__ Confirm_(
+		const ntvstr::string___ &Message,
+		const char *MessageLanguage,	// If != 'NULL', 'Message' is translates, otherwise it is displayed as is.
+		proxy__ &Proxy,
+		const char *CloseTextLanguage )
+	{
+		bso::bool__ Confirmation = false;
+	qRH
+		str::string XML, XSL;
+	qRB
+		XML.Init();
+		XSL.Init();
+
+		SetXMLAndXSL_( Message, MessageLanguage, XML, XSL );
+
+		Confirmation = Confirm( XML, XSL, ntvstr::string___(), Proxy, CloseTextLanguage );
+	qRR
+	qRT
+	qRE
+		return Confirmation;
+	}
+}
+
+bso::bool__ sclxdhtml::Confirm(
+	const ntvstr::string___ &RawMessage,
+	const char *Language,
+	proxy__ &Proxy )
+{
+	return Confirm_( RawMessage,  Language, Proxy, Language );
+}
+
+bso::bool__ sclxdhtml::Confirm(
+	const ntvstr::string___ &Message,
+	proxy__ &Proxy,
+	const char *Language )
+{
+	return Confirm_( Message,  NULL, Proxy, Language );
+}
+
+void sclxdhtml::HandleError(
+	proxy__ &Proxy,
+	const char *Language )
+{
+qRH
+	str::string Message;
+	err::buffer__ ErrBuffer;
+qRB
+	switch ( ERRType ) {
+	case err::t_Abort:
+		Message.Init();
+		if ( sclerror::GetPendingErrorTranslation( Language, Message, err::hUserDefined ) ) {
+			sclerror::ResetPendingError();
+			Alert( Message, Proxy, Language );
+		} 
+		break;
+	case err::t_Free:
+	case err::t_Return:
+		Alert( "???", Proxy, Language );
+		break;
+	default:
+		Alert( err::Message( ErrBuffer ), Proxy, Language );
+		break;
+	}
+
+	ERRRst();
+qRR
+qRT
+qRE
+}
+
+
+#if 0
+bso::bool__ sclxhtml::session___::XDHCBKXDHCBKLaunch(
+	const char *Id,
+	const char *Action )
+{
+	bso::bool__ Success = false;
+qRH
+	str::string Message;
+	err::buffer__ ErrBuffer;
+qRB
+		if ( _OnBeforeAction( Id, Action ) )
+			if ( !strcmp( Action, xdhcbk::CloseActionLabel ) )
+				Success = _OnClose();	// Dans ce cas, si 'Success' est  'false', la fermeture de l'application est suspendue.
+			else
+				Success = _Handler.Launch( Id, Action );
+qRR
+#if 0
+	switch ( ERRType ) {
+	case err::t_Abort:
+		Message.Init();
+		if ( sclerror::GetPendingErrorTranslation( _L(), Message, err::hUserDefined ) ) {
+			sclerror::ResetPendingError();
+			_A().RawAlert( Message );
+		} 
+		break;
+	case err::t_Free:
+	case err::t_Return:
+		_A().RawAlert( "???" );
+		break;
+	default:
+		_A().RawAlert( err::Message( ErrBuffer ) );
+		break;
+	}
+
+	ERRRst();
+# endif
+qRT
+qRE
+	return Success;
+}
+#endif
+/*
+void sclxdhtml::LaunchProject(
+	 proxy__ &Proxy,
+	sclfrntnd::kernel___ &Kernel )
+{
+qRH
+	str::string BackendFeature;
+qRB
+	BackendFeature.Init();
+	sclfrntnd::LaunchProject( Kernel, xdhdws::login::GetBackendFeatures( Proxy, BackendFeature ), BackendFeature );
+qRR
+qRT
+qRE
+}
+
+*/
+void sclxdhtml::prolog::GetContent(
+	sclfrntnd::frontend___ &Frontend,
+	xml::writer_ &Writer)
+{
+	sclfrntnd::GetProjectsFeatures( Frontend.Language(), Writer );
+}
+
+static sclmisc::project_type__ GetProjectType_( proxy__ &Proxy )
+{
+	sclmisc::project_type__ ProjectType = sclmisc::pt_Undefined;
+qRH
+	str::string Value;
+qRB
+	Value.Init();
+	ProjectType = sclmisc::GetProjectType( Proxy.GetContent( prolog::ProjectTypeId, Value ) );
+qRR
+qRT
+qRE
+	return ProjectType;
+}
+
+void sclxdhtml::prolog::GetContext(
+	proxy__ &Proxy,
+	xml::writer_ &Writer)
+{
+	Writer.PushTag( "ProjectType ");
+
+	Writer.PutValue( sclmisc::GetLabel( GetProjectType_( Proxy ) ) );
+
+	Writer.PopTag();
+}
+
+void sclxdhtml::prolog::DisplaySelectedProjectFilename(
+	proxy__ &Proxy,
+	const char *Id )
+{
+qRH
+	TOL_CBUFFER___ Buffer;
+	str::string FileName;
+	xdhcbk::args Args;
+	xdhcbk::retriever__ Retriever;
+qRB
+	Args.Init();
+	xdhcbk::Split( str::string( Proxy.GetResult( Id, Buffer ) ), Args );
+
+	Retriever.Init( Args );
+
+	FileName.Init();
+
+	if ( Retriever.Availability() != strmrg::aNone )
+		Retriever.GetString( FileName );
+
+	if ( FileName.Amount() != 0 )
+		Proxy.SetContent( RemoteProjectId, FileName );
+qRR
+qRT
+qRE
+}
+
+sclmisc::project_type__ sclxdhtml::prolog::GetProjectFeatures(
+	proxy__ &Proxy,
+	str::string_ &Feature )
+{
+	sclmisc::project_type__ Type = sclmisc::pt_Undefined;
+qRH
+	TOL_CBUFFER___ Buffer;
+qRB
+	switch ( Type = GetProjectType_( Proxy ) ) {
+	case sclmisc::ptNew:
+		break;
+	case sclmisc::ptRemote:
+		Feature.Append( Proxy.GetContent( RemoteProjectId, Buffer ) );
+		break;
+	case sclmisc::ptEmbedded:
+		qRVct();	// Not implemented yet.
+		break;
+	case sclmisc::ptPredefined:
+		Feature.Append( Proxy.GetContent( PredefinedProjectId, Buffer ) );
+		break;
+	default:
+		qRFwk();
+		break;
+	}
+qRR
+qRT
+qRE
+	return Type;
+}
+
+void sclxdhtml::prolog::LoadProject( proxy__ &Proxy )
+{
+qRH
+	str::string ProjectFeature;
+qRB
+	ProjectFeature.Init();
+	sclfrntnd::LoadProject( prolog::GetProjectFeatures( Proxy, ProjectFeature ), ProjectFeature );
+qRR
+qRT
+qRE
+}
+
+void sclxdhtml::login::GetContent(
+	sclfrntnd::frontend___ &Frontend,
+	xml::writer_ &Writer)
+{
+	sclfrntnd::GetBackendsFeatures(Frontend.Language(), Writer );
+}
+
+static frdbse::backend_type__ GetBackendType_( proxy__ &Proxy )
+{
+	frdbse::backend_type__ BackendType = frdbse::bt_Undefined;
+qRH
+	str::string Value;
+qRB
+	Value.Init();
+	BackendType = frdbse::GetBackendType( Proxy.GetContent( login::BackendTypeId, Value ) );
+qRR
+qRT
+qRE
+	return BackendType;
+}
+
+void sclxdhtml::login::GetContext(
+	proxy__ &Proxy,
+	xml::writer_ &Writer )
+{
+	Writer.PushTag( "BackendType" );
+
+	Writer.PutValue( frdbse::GetLabel( GetBackendType_( Proxy ) ) );
+
+	Writer.PopTag();
+}
+
+frdbse::backend_type__ sclxdhtml::login::GetBackendFeatures(
+	proxy__ &Proxy,
+	str::string_ &Feature )
+{
+	frdbse::backend_type__ Type = frdbse::bt_Undefined;
+qRH
+	TOL_CBUFFER___ Buffer;
+qRB
+	switch ( Type = GetBackendType_( Proxy ) ) {
+	case frdbse::btNone:
+		break;
+	case frdbse::btRemote:
+		Feature.Append( Proxy.GetContent( RemoteBackendId, Buffer ) );
+		break;
+	case frdbse::btEmbedded:
+		Feature.Append( Proxy.GetContent( EmbeddedBackendId, Buffer ) );
+		break;
+	case frdbse::btPredefined:
+		Feature.Append( Proxy.GetContent( PredefinedBackendId, Buffer ) );
+		break;
+	default:
+		qRFwk();
+		break;
+	}
+qRR
+qRT
+qRE
+	return Type;
+}
+
+void sclxdhtml::login::DisplaySelectedEmbeddedBackendFilename(
+	proxy__ &Proxy,
+	const char *Id )
+{
+qRH
+	TOL_CBUFFER___ Buffer;
+	str::string FileName;
+	xdhcbk::args Args;
+	xdhcbk::retriever__ Retriever;
+qRB
+	Args.Init();
+	xdhcbk::Split( str::string( Proxy.GetResult( Id, Buffer ) ), Args );
+
+	Retriever.Init( Args );
+
+	FileName.Init();
+
+	if ( Retriever.Availability() != strmrg::aNone )
+		Retriever.GetString( FileName );
+
+	if ( FileName.Amount() != 0 )
+		Proxy.SetContent( EmbeddedBackendId, FileName );
+qRR
+qRT
+qRE
+}
+
---
+++ stable/sclxdhtml.h
@@ -0,0 +1,507 @@
+/*
+	Copyright (C) 2000-2015 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of the Epeios framework.
+
+	The Epeios framework is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	The Epeios framework is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef SCLXDHTML__INC
+# define SCLXDHTML__INC
+
+# define SCLXDHTML_NAME		"SCLXDHTML"
+
+# if defined( E_DEBUG ) && !defined( SCLXDHTML_NODBG )
+#  define SCLXDHTML_DBG
+# endif
+
+// SoCLe X(SL)/DH(TML)
+
+# include "xdhdws.h"
+
+# include "sclfrntnd.h"
+
+# include "sclrgstry.h"
+# include "sclmisc.h"
+
+# include "err.h"
+
+# define SCLXDHTML_DEFAULT_SUFFIX "xdh"
+
+namespace sclxdhtml {
+	const sclrgstry::registry_ &GetRegistry( void );
+
+	const char *GetLauncher( void );
+
+	template <typename session> class action_callback__
+	{
+	private:
+		const char *_Name;
+	protected:
+		virtual void SCLXDHTMLLaunch(
+			session &Session,
+			const char *Id ) = 0;
+	public:
+		void reset( bso::bool__ = true )
+		{
+			_Name = NULL;
+		}
+		E_CVDTOR( action_callback__ );
+		void Init( const char *Name )
+		{
+			_Name = Name;
+		}
+		void Launch(
+			session &Session,
+			const char *Id )
+		{
+			return SCLXDHTMLLaunch( Session, Id );
+		}
+		const char *Name( void ) const
+		{
+			if ( _Name == NULL )
+				qRFwk();
+
+			return _Name;
+		}
+	};
+
+	qROW( crow__ );	// callback row;
+
+	template <typename session> E_TTCLONE_( bch::E_BUNCHt_( action_callback__<session> *, crow__ ), action_callbacks_ );
+
+	template <typename session> class action_handler_
+	{
+	private:
+		action_callback__<session> *_Get( const str::string_ &Action ) const
+		{
+			crow__ Row = stsfsm::GetId( Action, Automat );
+
+			if ( Row == qNIL )
+				return NULL;
+
+			return Callbacks( Row );
+		}
+	public:
+		struct s {
+			stsfsm::automat_::s Automat;
+			typename action_callbacks_<session>::s Callbacks;
+		};
+		stsfsm::automat_ Automat;
+		action_callbacks_<session> Callbacks;
+		action_handler_( s &S )
+		: Automat( S.Automat ),
+			Callbacks( S.Callbacks )
+		{}
+		void reset( bso::bool__ P = true )
+		{
+			Automat.reset( P );
+			Callbacks.reset( P );
+		}
+		void plug( qAS_ &AS )
+		{
+			Automat.plug( AS );
+			Callbacks.plug( AS );
+		}
+		action_handler_ &operator =(const action_handler_ &AH )
+		{
+			Automat = AH.Automat;
+			Callbacks = AH.Callbacks;
+
+			return *this;
+		}
+		void Init( void )
+		{
+			Automat.Init();
+			Callbacks.Init();
+		}
+		bso::bool__ Add(
+			const char *Name,
+			action_callback__<session> &Callback )
+		{
+			return stsfsm::Add( Name, *Callbacks.Append( &Callback ), Automat ) == stsfsm::UndefinedId;
+		}
+		void Launch(
+			session &Session,
+			const char *Id,
+			const char *Action )
+		{
+			action_callback__<session> *Callback = _Get( str::string(  Action ) );
+
+			if ( Callback == NULL )
+				qRFwk();	// L'action affecte  un vnement n'existe pas. Contrler le fichier '.xsl'.
+
+			return Callback->Launch( Session, Id );
+		}
+	};
+
+	E_AUTO1( action_handler );
+
+	template <typename session> class action_helper_callback__
+	{
+	protected:
+		virtual bso::bool__ SCLXHTMLOnBeforeAction(
+			session &Session,
+			const char *Id,
+			const char *Action ) = 0;
+		virtual bso::bool__ SCLXHTMLOnClose( session &Session ) = 0;
+	public:
+		void reset( bso::bool__ = true )
+		{
+			// Standardisation.
+		}
+		E_CVDTOR( action_helper_callback__ );
+		void Init( void )
+		{
+			// Standardisation.
+		}
+		bso::bool__ OnBeforeAction(
+			session &Session,
+			const char *Id,
+			const char *Action )
+		{
+			return SCLXHTMLOnBeforeAction( Session, Id, Action );
+		}
+		bso::bool__ OnClose( session &Session )
+		{
+			return SCLXHTMLOnClose( Session );
+		}
+	};
+
+	typedef fblfrd::reporting_callback__ _reporting_callback__;
+
+	typedef xdhcbk::session_callback__ _session_callback__;
+
+	using xdhdws::proxy__;
+
+	void Alert(
+		const ntvstr::string___ &XML,
+		const ntvstr::string___ &XSL,
+		const ntvstr::string___ &Title,
+		proxy__ &Proxy,
+		const char *Language );
+
+	void Alert(
+		const ntvstr::string___ &RawMessage,
+		const char *Language,
+		proxy__ &Proxy );	// Translates 'Message'.
+
+	void Alert(
+		const ntvstr::string___ &Message,
+		proxy__ &Proxy,
+		const char *Language );	// Displays 'Message' as is. 'Language' is used for the closing text message.
+
+	bso::bool__ Confirm(
+		const ntvstr::string___ &XML,
+		const ntvstr::string___ &XSL,
+		const ntvstr::string___ &Title,
+		proxy__ &Proxy,
+		const char *Language );
+
+	bso::bool__ Confirm(
+		const ntvstr::string___ &RawMessage,
+		const char *Language,
+		proxy__ &Proxy );
+
+	bso::bool__ Confirm(
+		const ntvstr::string___ &Message,
+		proxy__ &Proxy,
+		const char *Language );	// Displays 'Message' as is. 'Language' is used for the closing text message.
+
+
+	class reporting_callback__
+	: public _reporting_callback__
+	{
+	private:
+		Q37_MRMDF( proxy__, P_, Proxy_ );
+		Q37_MPMDF( const char, L_, Language_ );
+	protected:
+		virtual void FBLFRDReport(
+			fblovl::reply__ Reply,
+			const char *Message ) override
+		{
+			Alert( Message, P_(), L_() );
+		}
+	public:
+		void reset( bso::bool__ P = true )
+		{
+			_reporting_callback__::reset( P );
+			Proxy_ = NULL;
+		}
+		E_CVDTOR( reporting_callback__ );
+		void Init(
+			proxy__ &Proxy,
+			const char *Language )
+		{
+			_reporting_callback__::Init();
+			Proxy_ = &Proxy;
+			Language_ = Language;
+		}
+	};
+
+	void HandleError(
+		proxy__ &Proxy,
+		const char *Language );
+
+	// L'utilisateur met dans le type 'instances' ses propres objets et instancie le tout par un 'new' (en surchargeant 'SCLXHTMLNew(...)', et il est assur qu'un 'delete' sera fait une fois la bibliothque dcharge.
+	template <typename instances, typename frontend, typename page, page UndefinedPage > class session___
+	: public _session_callback__,
+	  public proxy__,
+	  public instances,
+	  public frontend
+	{
+	private:
+		page _Page;	// Current page;
+		reporting_callback__ _ReportingCallback;
+	protected:
+		virtual void SCLXDHTMLRefresh( page Page ) = 0;
+	public:
+		void reset( bso::bool__ P = true )
+		{
+			_session_callback__::reset( P );
+			instances::reset( P );
+			frontend::reset( P );
+			_Page = UndefinedPage;
+			_ReportingCallback.reset( P );
+		}
+		E_CVDTOR( session___ )
+		void Init(
+			const char *Language,
+			xdhcbk::proxy_callback__ *Callback )
+		{
+			proxy__::Init( Callback );
+			_ReportingCallback.Init( *this, Language );
+			frontend::Init( Language, _ReportingCallback, sclmisc::GetRegistry() );
+			_session_callback__::Init();
+			_Page = UndefinedPage;
+			// instances::Init( *this );	// Made on connection.
+		}
+		bso::bool__ Connect(
+			const fblfrd::compatibility_informations__ &CompatibilityInformations,
+			fblfrd::incompatibility_informations_ &IncompatibilityInformations )
+		{
+			if ( !frontend::Connect( CompatibilityInformations, IncompatibilityInformations ) )
+				return false;
+
+			if ( frontend::IsConnected() )
+				instances::Init( *this );
+			
+			return true;
+		}
+		const char *Language( void )
+		{
+			return frontend::Language();
+		}
+		void Refresh( void )
+		{
+			if ( _Page == UndefinedPage )
+				qRFwk();
+			else
+				SCLXDHTMLRefresh( _Page );
+		}
+		void SwitchTo( page Page = UndefinedPage )
+		{
+			if ( Page != UndefinedPage )
+				_Page = Page;
+			else
+				qRFwk();
+		}
+		const str::string_ &GetTranslation(
+			const char *Message,
+			str::string_ &Translation )
+		{
+			return scllocale::GetTranslation( Message, Language(), Translation );
+		}
+		void AlertU( const ntvstr::string___ &Message )	// Displays 'Message' as is.
+		{
+			sclxdhtml::Alert ( Message, *this, Language() );
+		}
+		void AlertT( const ntvstr::string___ &RawMessage )	// Translates 'RawMessage'.
+		{
+			sclxdhtml::Alert ( RawMessage, Language(), *this );
+		}
+		void Alert(
+			const ntvstr::string___ &XML,
+			const ntvstr::string___ &XSL,
+			const ntvstr::string___ &Title )
+		{
+			sclxdhtml::Alert( XML, XSL, Title, *this, Language() );
+		}
+		bso::bool__ ConfirmU( const ntvstr::string___ &Message )	// Displays 'Message' as is.
+		{
+			return sclxdhtml::Confirm( Message, *this, Language() );
+		}
+		bso::bool__ ConfirmT( const ntvstr::string___ &RawMessage )	// Translates 'RawMessage'.
+		{
+			return sclxdhtml::Confirm( RawMessage, Language(), *this );
+		}
+		bso::bool__ Confirm(
+			const ntvstr::string___ &XML,
+			const ntvstr::string___ &XSL,
+			const ntvstr::string___ &Title )
+		{
+			return sclxdhtml::Confirm( XML, XSL, Title, this, Language() );
+		}
+	};
+
+	// L'utilisateur met dans le type 'instances' ses propres objets et instancie le tout par un 'new' (en surchargeant 'SCLXHTMLNew(...)', et il est assur qu'un 'delete' sera fait une fois la bibliothque dcharge.
+	template <typename session> class core___
+	{
+	private:
+		action_handler<session> _Handler;
+		frdbse::mode__ _Mode;
+		Q37_MRMDF( action_helper_callback__<session>, _AH, _ActionHelperCallback );
+		bso::bool__ _OnBeforeAction(
+			session &Session,
+			const char *Id,
+			const char *Action )
+		{
+			return _AH().OnBeforeAction( Session, Id, Action );
+		}
+		bso::bool__ _OnClose( session &Session )
+		{
+			return _AH().OnClose( Session );
+		}
+	public:
+		void reset( bso::bool__ P = true )
+		{
+			_Handler.reset( P );
+			_Mode = frdbse::m_Undefined;
+			_ActionHelperCallback = NULL;
+		}
+		E_CVDTOR( core___ )
+		void Init(
+			frdbse::mode__ Mode,
+			action_helper_callback__<session> &ActionHelperCallback )
+		{
+			_ActionHelperCallback = &ActionHelperCallback;
+			_Mode = Mode;
+			_Handler.Init();
+		}
+		void AddActionCallback(
+			const char *ActionName,
+			action_callback__<session> &Callback )
+		{
+			_Handler.Add( ActionName, Callback );
+		}
+		bso::bool__ Launch(
+			session &Session,
+			const char *Id,
+			const char *Action )
+		{
+			bso::bool__ Success = true;
+		qRH
+			TOL_CBUFFER___ Buffer;
+		qRB
+			if ( _OnBeforeAction( Session, Id, Action ) )
+				if ( !strcmp( Action, xdhcbk::CloseActionLabel ) )
+					Success = _OnClose( Session );	// Dans ce cas, si 'Success' est  'false', la fermeture de l'application est suspendue.
+				else
+# if 0
+					Success = _Handler.Launch( Session, Id, Action );
+# else
+					_Handler.Launch( Session, Id, Action );
+# endif
+		qRR
+			HandleError( Session, Session.Language() );
+		qRT
+		qRE
+			return Success;
+		}
+		E_RODISCLOSE__( frdbse::mode__, Mode );
+	};
+
+
+	/*********************************/
+
+	inline void LoadXSLAndTranslateTags(
+		const rgstry::tentry__ &FileName,
+		const sclrgstry::registry_ &Registry,
+		str::string_ &String,
+		bso::char__ Marker = '#' )
+	{
+		sclmisc::LoadXMLAndTranslateTags( FileName, Registry, String, Marker );
+	}
+
+	/*
+	void LoadProject( proxy__ &Proxy );
+
+	void LaunchProject(
+		proxy__ &Proxy ,
+		sclfrntnd::kernel___ &Kernel );
+		*/
+
+	void SCLXDHTMLInitialization( frdbse::mode__ Mode );	// To overload.
+
+	xdhcbk::session_callback__ *SCLXDHTMLNew(
+		const char *Language,
+		xdhcbk::proxy_callback__ *ProxyCallback );	// To override.
+
+	inline void Connect(
+		frdbse::backend_type__ BackendType,
+		const str::string_ &BackendFeature )
+	{
+		return sclfrntnd::Connect( BackendType, BackendFeature );
+	}
+
+	namespace prolog {
+		E_CDEF( char *, ProjectTypeId, "ProjectType" );
+		E_CDEF( char *, PredefinedProjectId, "PredefinedProject" );
+		E_CDEF( char *, RemoteProjectId, "RemoteProject" );
+
+		void GetContent(
+			sclfrntnd::frontend___ &Frontend,
+			xml::writer_ &Writer );
+
+		void GetContext(
+			proxy__ &Proxy,
+			xml::writer_ &Writer );
+
+		void DisplaySelectedProjectFilename(
+			proxy__ &Proxy,
+			const char *Id );
+
+		sclmisc::project_type__ GetProjectFeatures(
+			proxy__ &Proxy,
+			str::string_ &Feature );
+
+		void LoadProject( proxy__ &Proxy );
+	}
+
+	namespace login {
+		E_CDEF( char *, BackendTypeId, "BackendType" );
+		E_CDEF( char *, RemoteBackendId, "RemoteBackend" );
+		E_CDEF( char *, EmbeddedBackendId, "EmbeddedBackend" );
+		E_CDEF( char *, PredefinedBackendId, "PredefinedBackend" );
+
+		void GetContent(
+			sclfrntnd::frontend___ &Frontend,
+			xml::writer_ &Writer );
+
+		void GetContext(
+			proxy__ &Proxy,
+			xml::writer_ &Writer );
+
+		frdbse::backend_type__ GetBackendFeatures(
+			proxy__ &Proxy,
+			str::string_ &Feature );
+
+		void DisplaySelectedEmbeddedBackendFilename(
+			proxy__ &Proxy,
+			const char *Id );
+	}
+
+}
+
+#endif
