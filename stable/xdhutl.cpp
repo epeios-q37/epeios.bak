0cd9f05b9f249e614fd812b6fd1ba59e278f6e91
csimon <epeios-q37@users.noreply.github.com>
2015-10-27 16:26:17 +0100

    27/10/2015 16:26:17

---
+++ stable/xdhutl.cpp
@@ -0,0 +1,707 @@
+/*
+	Copyright (C) 2000-2015 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of the Epeios framework.
+
+	The Epeios framework is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	The Epeios framework is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#define XDHUTL__COMPILATION
+
+#include "xdhutl.h"
+
+using namespace xdhutl;
+
+namespace {
+	stsfsm::automat ActionAutomat_;
+
+	template <typename type> inline type GetId_(
+		const nstring___ &Pattern,
+		const stsfsm::automat_ &Automat,
+		type UndefinedValue,
+		bso::uint__ Amount )
+	{
+		type Id = UndefinedValue;
+	qRH
+		str::string Buffer;
+	qRB
+		Buffer.Init();
+		Id = stsfsm::GetId( Pattern.UTF8( Buffer ), Automat, UndefinedValue, Amount );
+	qRR
+	qRT
+	qRE
+		return Id;
+	}
+
+	static void InitAndFillActionAutomat_( void )
+	{
+		ActionAutomat_.Init(),
+		stsfsm::Fill( ActionAutomat_, a_amount, GetLabel );
+	}
+
+	void InitAndFillAutomats_( void )
+	{
+		InitAndFillActionAutomat_();
+	}
+}
+
+#define A( name )	case a##name : return #name; break
+
+const char *xdhutl::GetLabel( action__ Action )
+{
+	switch ( Action ) {
+		A( OpenFile );
+		A( OpenFiles );
+		A( SaveFile );
+	default:
+		qRFwk();
+		break;
+	}
+
+	return NULL;	// To avoid a 'warning'.
+}
+
+action__ xdhutl::GetAction( const nstring___ &Pattern )
+{
+	return GetId_( Pattern, ActionAutomat_, a_Undefined, a_amount );
+}
+
+namespace {
+	static action__ GetPredefinedAction_(
+		const xdhcmn::digest_ &ActionWithParameters,
+		str::string_ &UserAction,
+		xdhcmn::digest_ &Args )
+	{
+		action__ Action = a_Undefined;
+	qRH
+		xdhcmn::retriever__ Retriever;
+		str::string PredefinedAction;
+	qRB
+		if ( ActionWithParameters.Amount() < 2 )
+			qRFwk();
+
+		Retriever.Init( ActionWithParameters );
+
+		PredefinedAction.Init();
+		Retriever.GetString( PredefinedAction );
+		Action = GetAction( PredefinedAction );
+
+		Retriever.GetString( UserAction );
+
+		if ( Retriever.Availability() != strmrg::aNone )
+			Retriever.GetTable( Args );
+	qRR
+	qRT
+	qRE
+		return Action;
+	}
+
+	action__ GetAction_(
+		const xdhcmn::digest_ &ActionWithParameters,
+		str::string_ &UserAction,
+		xdhcmn::digest_ &Args )
+	{
+		strmrg::retriever__ Retriever;
+
+		Retriever.Init( ActionWithParameters );
+
+		switch ( ActionWithParameters.Amount() ) {
+		case 1:
+			Retriever.GetString( UserAction );
+			return a_User;
+		case 2:
+		case 3:
+			return GetPredefinedAction_( ActionWithParameters, UserAction, Args );
+			break;
+		default:
+			qRFwk();
+			break;
+		}
+
+		return a_Undefined;	// To avoid a 'warning'.
+	}
+}
+
+inline void HandleKeyShortcutModifier_(
+	char Char,
+	const str::string_ &Pattern,
+	str::string_ &Shortcut )
+{
+	sdr::row__ CharPos = qNIL, PlusPos = qNIL;
+
+	if ( ( ( CharPos = Pattern.Search(Char) ) != qNIL ) 
+		 && ( ( PlusPos = Pattern.Search( '+' ) ) != qNIL )
+		 && ( *CharPos < *PlusPos ) )
+		Shortcut.Append( Char );
+	else
+		Shortcut.Append( '.' );
+}
+
+void xdhutl::BuildKeyShortcut(
+	const str::string_ &Keys,
+	str::string_ &Shortcut )
+{
+	sdr::row__ PlusPos = Keys.Search( '+' );
+
+	if ( PlusPos != qNIL ) {
+		if ( Keys.Amount() != 1 ) {
+
+			if ( PlusPos == Keys.Last() )
+				qRFwk();
+
+			HandleKeyShortcutModifier_( 'A', Keys, Shortcut );	// Alt
+			HandleKeyShortcutModifier_( 'S', Keys, Shortcut );	// Shift
+			HandleKeyShortcutModifier_( 'C', Keys, Shortcut );	// Control
+			HandleKeyShortcutModifier_( 'M', Keys, Shortcut );	// Meta
+
+			Shortcut.Append( Keys, Keys.Next( PlusPos ) ); 
+		} else // In this case, the shortcut is simply a '+', os the '+' is not the separator next to modifiers.
+			Shortcut.Append( Keys );
+	} else
+		Shortcut.Append( Keys );
+}
+
+void xdhutl::FillEventAbstract(
+	const str::string_ &DefaultEvent,
+	const xdhcmn::digest_ &Description,
+	event_abstract_ &Abstract )
+{
+qRH
+	xdhcmn::digest ActionWithParameters;
+	xdhcmn::retriever__ Retriever;
+	str::string Keys;
+	bso::bool__ RetrieveKeys = false;
+	str::string KeyShortcut;
+	sdr::row__ PlusPos = qNIL;
+qRB
+	Retriever.Init( Description );
+	Keys.Init();
+
+	switch ( Description.Amount() ) {
+	case 3:
+		RetrieveKeys = true;
+	case 2:
+		Retriever.GetString( Abstract.Event );
+	case 1:
+		ActionWithParameters.Init();
+		Retriever.GetTable( ActionWithParameters );
+		Abstract.S_.Action = GetAction_( ActionWithParameters, Abstract.UserAction, Abstract.Args );
+		break;
+	default:
+		qRFwk();
+		break;
+	}
+
+	if ( RetrieveKeys ) {
+		if ( !IsKeyEvent( Abstract.Event ) )
+			qRFwk();
+
+		Keys.Init();
+		Retriever.GetString( Keys );
+
+		KeyShortcut.Init();
+
+		BuildKeyShortcut( Keys, KeyShortcut );
+
+		Abstract.EventKeys = KeyShortcut;
+	}
+
+	if ( Abstract.Event.Amount() == 0 )
+		Abstract.Event = DefaultEvent;
+qRR
+qRT
+qRE
+}
+
+void xdhutl::FillEventAbstractsMono(
+	const str::string_ &DefaultEvent,
+	const xdhcmn::digest_ &Description,
+	event_abstracts_ &Abstracts )
+{
+qRH
+	event_abstract Abstract;
+qRB
+	Abstract.Init();
+
+	FillEventAbstract( DefaultEvent, Description, Abstract );
+
+	Abstracts.Append( Abstract );
+qRR
+qRT
+qRE
+}
+
+amount__ xdhutl::FillEventAbstractsMulti(
+	const str::string_ &DefaultEvent,
+	const xdhcmn::digest_ &Descriptions,
+	event_abstracts_ &Abstracts )
+{
+	amount__ Amount = 0;
+qRH
+	xdhcmn::digest Definition;
+	strmrg::retriever__ Retriever;
+qRB
+	Retriever.Init( Descriptions );
+
+	while ( Retriever.Availability() != strmrg::aNone ) {
+		if ( Amount == AmountMax )
+			qRLmt();
+
+		Amount++;
+
+		Definition.Init();
+
+		Retriever.GetTable( Definition );
+
+		FillEventAbstractsMono( DefaultEvent, Definition, Abstracts );
+	}
+qRR
+qRT
+qRE
+	return Amount;
+}
+
+amount__ xdhutl::FillEventAbstracts(
+	const str::string_ &TagName,
+	const xdhcmn::digest_ &Descriptions,
+	event_abstracts_ &Abstracts )
+
+{
+	amount__ Amount = 0;
+qRH
+	str::string DefaultEvent;
+	TOL_CBUFFER___ Buffer;
+qRB
+	DefaultEvent.Init();
+	GetTagDefaultEvent( TagName, DefaultEvent );
+
+	Amount = FillEventAbstractsMulti( DefaultEvent, Descriptions, Abstracts );
+qRR
+qRT
+qRE
+	return Amount;
+}
+
+amount__ xdhutl::FillEventAbstracts(
+	const xdhcmn::digest_ &Descriptions,
+	str::string_ &Id,
+	event_abstracts_ &Abstracts )
+{
+	amount__ Amount = 0;
+qRH
+	xdhcmn::retriever__ Retriever;
+	xdhcmn::digest EventsDescriptions;
+	str::string TagName;
+qRB
+	Retriever.Init( Descriptions );
+
+	Retriever.GetString( Id );
+
+	TagName.Init();
+	Retriever.GetString( TagName );
+
+	EventsDescriptions.Init();
+	Retriever.GetTable( EventsDescriptions );
+
+	Amount = FillEventAbstracts( TagName, EventsDescriptions, Abstracts );
+qRR
+qRT
+qRE
+	return Amount;
+}
+
+void xdhutl::FillEventAbstracts(
+	const xdhcmn::digest_ &Descriptions,
+	str::strings_ &Ids,
+	event_abstracts_ &Abstracts )
+{
+qRH
+	xdhcmn::retriever__ Retriever;
+	xdhcmn::digest SubDescriptions;
+	str::string Id;
+	TOL_CBUFFER___ Buffer;
+	amount__ Amount = 0;
+qRB
+	Retriever.Init( Descriptions );
+
+	while ( Retriever.Availability() != strmrg::aNone ) {
+		SubDescriptions.Init();
+		Retriever.GetTable( SubDescriptions );
+
+		Id.Init();
+		Amount = FillEventAbstracts( SubDescriptions, Id, Abstracts );
+
+		while ( Amount-- )
+			Ids.Append( Id );
+	}
+qRR
+qRT
+qRE
+}
+
+bso::bool__ xdhutl::Exists(
+	const char *Event,
+	const event_abstracts_ &Abstracts )
+{
+	ctn::E_CITEM( event_abstract_ ) Abstract;
+	sdr::row__ Row = Abstracts.First();
+
+	Abstract.Init( Abstracts );
+
+	while ( ( Row != qNIL ) && ( Abstract( Row ).Event != Event ) )
+		Row = Abstracts.Next( Row );
+
+	return Row != qNIL;
+}
+
+sdr::row__ xdhutl::Find(
+	const str::string_ &Event,
+	const str::string_ &RawKeys,
+	const event_abstracts_ &Abstracts )
+{
+	sdr::row__ Row = qNIL;
+qRH
+	ctn::E_CITEM( event_abstract_ ) Abstract;
+	str::string Keys;
+qRB
+	Row = Abstracts.First();
+
+	Abstract.Init( Abstracts );
+
+	Keys.Init();
+	BuildKeyShortcut( RawKeys, Keys );
+
+	while ( ( Row != qNIL )
+		    && ( ( Abstract( Row ).Event != Event )
+			     || ( IsKeyEvent( Event ) && Abstract( Row ).EventKeys != Keys ) ) )
+		Row = Abstracts.Next( Row );
+
+	qRR
+qRT
+qRE
+	return Row;
+}
+
+bso::bool__ xdhutl::FetchEventAbstract(
+	const xdhcmn::digest_ &Digest,
+	str::string_ &Id,
+	event_abstract_ &AbstractBuffer )
+{
+	bso::bool__ Found = false;
+qRH
+	xdhcmn::retriever__ Retriever;
+	str::string  Name, Type, Keys;
+	xdhcmn::digest Events;
+	xdhutl::event_abstracts Abstracts;
+	ctn::E_CITEM( xdhutl::event_abstract_ ) Abstract;
+	sdr::row__ Row = qNIL;
+qRB
+	Retriever.Init( Digest );
+
+	Id.Init();
+	Retriever.GetString( Id );
+
+	Name.Init();
+	Retriever.GetString( Name );
+
+	Type.Init();
+	Retriever.GetString( Type );
+
+	Keys.Init();
+	Retriever.GetString( Keys );
+
+	Events.Init();
+	Retriever.GetTable( Events );
+
+	Abstracts.Init();
+	xdhutl::FillEventAbstracts( Name, Events, Abstracts );
+
+	Row = xdhutl::Find( Type, Keys, Abstracts );
+
+	if ( Row == qNIL ) {
+		if ( !xdhutl::IsKeyEvent( Type ) )
+			qRGnr();
+
+		qRReturn;
+	}
+
+	Found = true;
+
+	Abstract.Init( Abstracts );
+	AbstractBuffer = Abstract( Row );	// To set the position of 'Handler'.
+qRR
+qRT
+qRE
+	return Found;
+}
+
+bso::bool__ xdhutl::FetchEventAbstract(
+	const str::string_ &RawDigest,
+	str::string_ &Id,
+	event_abstract_ &Abstract )
+{
+	bso::bool__ Found = false;
+qRH
+	xdhcmn::digest Digest;
+qRB
+	Digest.Init();
+	xdhcmn::Split( RawDigest, Digest );
+
+	Found = FetchEventAbstract( Digest, Id, Abstract );
+qRR
+qRT
+qRE
+	return Found;
+}
+
+void xdhutl::ExtractWidgetFeatures(
+	const xdhcmn::digest_ &Description,
+	str::string_ &Type,
+	str::string_ &Parameters,
+	str::string_ &ContentRetrievingMethod,
+	str::string_ &FocusingMethod )
+{
+qRH
+	xdhcmn::retriever__ Retriever;
+qRB
+	Retriever.Init( Description );
+
+	if ( Retriever.Availability() != strmrg::aNone )
+		Retriever.GetString( Type );
+
+	if ( Retriever.Availability() != strmrg::aNone )
+		Retriever.GetString( Parameters );
+
+	if ( Retriever.Availability() != strmrg::aNone )
+		Retriever.GetString( ContentRetrievingMethod );
+
+	if ( Retriever.Availability() != strmrg::aNone )
+		Retriever.GetString( FocusingMethod );
+qRR
+qRT
+qRE
+}
+
+void xdhutl::ExtractWidgetFeatures(
+	const str::string_ &RawDescription,
+	str::string_ &Type,
+	str::string_ &Parameters,
+	str::string_ &ContentRetrievingMethod,
+	str::string_ &FocusingMethod )
+
+{
+qRH
+	xdhcmn::digest Description;
+qRB
+	Description.Init();
+	xdhcmn::Split( RawDescription, Description );
+
+	ExtractWidgetFeatures( Description, Type, Parameters, ContentRetrievingMethod, FocusingMethod );
+qRR
+qRT
+qRE
+}
+
+
+void xdhutl::ExtractWidgetTypeAndParameters(
+	const xdhcmn::digest_ &Description,
+	str::string_ &Type,
+	str::string_ &Parameters )
+{
+qRH
+	str::string ContentRetrievingMethod, FocusingMethod;
+qRB
+	ContentRetrievingMethod.Init();
+	FocusingMethod.Init();
+	ExtractWidgetFeatures( Description, Type, Parameters, ContentRetrievingMethod, FocusingMethod );
+qRR
+qRT
+qRE
+}
+
+void xdhutl::ExtractWidgetContentRetrievingMethod(
+	const xdhcmn::digest_ &Description,
+	str::string_ &Method )
+{
+qRH
+	str::string Type, Parameters, OtherMethod;
+qRB
+	Type.Init();
+	Parameters.Init();
+	OtherMethod.Init();
+
+	ExtractWidgetFeatures( Description, Type, Parameters, Method, OtherMethod );
+qRR
+qRT
+qRE
+}
+
+void xdhutl::ExtractWidgetContentRetrievingMethod(
+	const str::string_ &Description,
+	str::string_ &Method )
+{
+qRH
+	str::string Type, Parameters, OtherMethod;
+qRB
+	Type.Init();
+	Parameters.Init();
+	OtherMethod.Init();
+
+	ExtractWidgetFeatures( Description, Type, Parameters, Method, OtherMethod );
+qRR
+qRT
+qRE
+}
+
+void xdhutl::ExtractWidgetFocusingMethod(
+	const xdhcmn::digest_ &Description,
+	str::string_ &Method )
+{
+qRH
+	str::string Type, Parameters, OtherMethod;
+qRB
+	Type.Init();
+	Parameters.Init();
+	OtherMethod.Init();
+
+	ExtractWidgetFeatures( Description, Type, Parameters, OtherMethod, Method );
+qRR
+qRT
+qRE
+}
+
+void xdhutl::ExtractWidgetFocusingMethod(
+	const str::string_ &Description,
+	str::string_ &Method )
+{
+qRH
+	str::string Type, Parameters, OtherMethod;
+qRB
+	Type.Init();
+	Parameters.Init();
+	OtherMethod.Init();
+
+	ExtractWidgetFeatures( Description, Type, Parameters, OtherMethod, Method );
+qRR
+qRT
+qRE
+}
+
+namespace {
+	void ExtractWidgetTypeAndParameters_(
+		const xdhcmn::digest_ &XDescription,	// Contains also the element id.
+		str::string_ &Id,
+		str::string_ &Type,
+		str::string_ &Parameters )
+	{
+	qRH
+		xdhcmn::digest Description;
+		xdhcmn::retriever__ Retriever;
+	qRB
+		Retriever.Init( XDescription );
+		
+		Retriever.GetString( Id );
+
+		Description.Init();
+		Retriever.GetTable( Description );
+
+		ExtractWidgetTypeAndParameters( Description, Type, Parameters );
+	qRR
+	qRT
+	qRE
+	}
+}
+
+void xdhutl::ExtractWidgetsTypesAndParametersSets(
+	const xdhcmn::digest_ &Descriptions,
+	str::strings_ &Ids,
+	str::strings_ &Types,
+	str::strings_ &ParametersSets )
+{
+qRH
+	xdhcmn::digest XDescription;
+	str::string Id, Type, Parameters;
+	xdhcmn::retriever__ Retriever;
+qRB
+	Retriever.Init( Descriptions );
+
+	while ( Retriever.Availability() != strmrg::aNone ) {
+
+		XDescription.Init();
+		Retriever.GetTable( XDescription );
+
+		Id.Init();
+		Type.Init();
+		Parameters.Init();
+
+		ExtractWidgetTypeAndParameters_( XDescription, Id, Type, Parameters );
+
+		Ids.Append( Id );
+		Types.Append( Type );
+		ParametersSets.Append( Parameters );
+	}
+qRR
+qRT
+qRE
+}
+
+
+
+void xdhutl::FillCasting(
+	const xdhcmn::digest_ &Description,
+	str::string_ &Id,
+	str::string_ &Casting )
+{
+qRH
+	xdhcmn::retriever__ Retriever;
+qRB
+	Retriever.Init( Description );
+
+	Retriever.GetString( Id );
+	Retriever.GetString( Casting );
+qRR
+qRT
+qRE
+}
+
+
+
+void xdhutl::FillCastings(
+	const xdhcmn::digest_ &Descriptions,
+	str::strings_ &Ids,
+	str::strings_ &Castings )
+{
+qRH
+	xdhcmn::retriever__ Retriever;
+	xdhcmn::digest Description;
+	str::string Id, Casting; 
+qRB
+	Retriever.Init( Descriptions );
+
+	while ( Retriever.Availability() != strmrg::aNone ) {
+		Description.Init();
+		Retriever.GetTable( Description );
+
+		Id.Init();
+		Casting.Init();
+		FillCasting( Description, Id, Casting );
+
+		Ids.Append( Id );
+		Castings.Append( Casting );
+	}
+qRR
+qRT
+qRE
+}
+
---
+++ stable/xdhutl.h
@@ -0,0 +1,251 @@
+/*
+	Copyright (C) 2000-2015 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of the Epeios framework.
+
+	The Epeios framework is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	The Epeios framework is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef XDHUTL__INC
+# define XDHUTL__INC
+
+# define XDHUTL_NAME		"XDHUTL"
+
+# if defined( E_DEBUG ) && !defined( XDHUTL_NODBG )
+#  define XDHUTL_DBG
+# endif
+
+// XD(HTML) Upstream TooLs
+
+# include "xdhcmn.h"
+
+# include "err.h"
+
+namespace xdhutl {
+	using xdhcmn::nchar__;
+	using xdhcmn::nstring___;
+
+	E_ENUM( action ) {
+		aOpenFile,
+		aOpenFiles,
+		aSaveFile,
+		a_amount,
+		a_Undefined,
+		a_User	// To report an action which is not a predefined one, but a user one.
+	};
+
+	const char *GetLabel( action__ Action );
+
+	action__ GetAction( const nstring___ &Pattern );
+
+	inline bso::bool__ IsPredefined( action__ Action )
+	{
+		return ( Action < a_amount );
+	}
+
+	inline bso::bool__ IsKeyEvent( const str::string_ &Event )
+	{
+		return  !str::Compare( Event, str::string( "key" ), 0, 0, 3 );
+	}
+
+	inline bso::bool__ IsKeyEvent( const char *Event )
+	{
+		return IsKeyEvent(str::string( Event ) );
+	}
+
+	void BuildKeyShortcut(
+		const str::string_ &Keys,
+		str::string_ &Shortcut );
+
+	class event_abstract_ {
+	public:
+		struct s {
+			str::string_::s Event;
+			str::string_::s EventKeys;
+			action__ Action;
+			str::string_::s UserAction;
+			xdhcmn::digest_::s Args;
+		} &S_;
+		str::string_ Event;
+		str::string_ EventKeys;	// For key-related events.
+		str::string_ UserAction;
+		xdhcmn::digest_ Args;	// Only for NON-user action (i.e. 'Action' != 'a_User').
+		event_abstract_( s &S )
+		: S_( S ),
+		  Event( S.Event ),
+		  EventKeys( S.EventKeys ),
+		  UserAction( S.UserAction ),
+		  Args( S.Args )
+		{}
+		void reset( bso::bool__ P = true )
+		{
+			S_.Action = a_Undefined;
+
+			Event.reset( P );
+			EventKeys.reset( P );
+			UserAction.reset( P );
+			Args.reset( P );
+		}
+		void plug( qAS_ &AS )
+		{
+			Event.plug( AS );
+			EventKeys.plug( AS );
+			UserAction.plug( AS );
+			Args.plug( AS );
+		}
+		event_abstract_ &operator =( const event_abstract_ &EA )
+		{
+			S_.Action = EA.S_.Action;
+
+			Event = EA.Event;
+			EventKeys = EA.EventKeys;
+			UserAction = EA.UserAction;
+			Args = EA.Args;
+
+			return *this;
+		}
+		void Init( void )
+		{
+			S_.Action = a_Undefined;
+
+			Event.Init();
+			EventKeys.Init();
+			UserAction.Init();
+			Args.Init();
+		}
+		E_RODISCLOSE_( action__, Action );
+	};
+
+	E_AUTO( event_abstract );
+
+	typedef ctn::E_CONTAINER_( event_abstract_ ) event_abstracts_;
+	E_AUTO( event_abstracts );
+
+	inline void GetTagDefaultEvent(
+		const str::string_ &Name,
+		str::string_ &Event )
+	{
+		if ( Name == "SELECT" )
+			Event.Append( "change" );
+		else
+			Event.Append( "click" );
+	}
+
+	void FillEventAbstract(
+		const str::string_ &DefaultEvent,
+		const xdhcmn::digest_ &Description,
+		event_abstract_ &Abstract);
+
+	void FillEventAbstractsMono(
+		const str::string_ &DefaultEvent,
+		const xdhcmn::digest_ &Description,
+		event_abstracts_ &Abstracts );
+
+	// For the amount of abstracts created.
+	typedef bso::u8__ amount__;
+
+	E_CDEF( amount__, AmountMax, bso::U8Max );
+
+	amount__ FillEventAbstractsMulti(
+		const str::string_ &DefaultEvent,
+		const xdhcmn::digest_ &Descriptions,
+		event_abstracts_ &Abstracts );
+
+	amount__ FillEventAbstracts(
+		const str::string_ &TagName,
+		const xdhcmn::digest_ &Descriptions,
+		event_abstracts_ &Abstracts );
+
+	amount__ FillEventAbstracts(
+		const xdhcmn::digest_ &Descriptions,
+		str::string_ &Id,
+		event_abstracts_ &Abstracts );
+
+	void FillEventAbstracts(
+		const xdhcmn::digest_ &Descriptions,
+		str::strings_ &Ids,
+		event_abstracts_ &Abstracts );
+
+	bso::bool__ Exists(
+		const char *Event,
+		const event_abstracts_ &Abstracts );
+
+	sdr::row__ Find(
+		const str::string_ &Event,
+		const str::string_ &Keys,	// Only for keyboard-related events.
+		const event_abstracts_ &Abstracts	);	// Returns the 'row' in 'Abstracts' corresponding to 'Event' ; 'qNIL' if not found.
+
+	bso::bool__ FetchEventAbstract(
+		const xdhcmn::digest_ &Digest,
+		str::string_ &Id,
+		event_abstract_ &Abstract );	// If false, no digest found because the event is a key-related event but not with recognized key sequence.
+
+	bso::bool__  FetchEventAbstract(
+		const str::string_ &Digest,
+		str::string_ &Id,
+		event_abstract_ &Abstract );	// If false, no digest found because the event is a key-related event but not with recognized key sequence.
+
+	void ExtractWidgetFeatures(
+		const xdhcmn::digest_ &Description,
+		str::string_ &Type,
+		str::string_ &Parameters,
+		str::string_ &ContentRetrievingMethod,
+		str::string_ &FocusingMethod );
+
+	void ExtractWidgetFeatures(
+		const str::string_ &Description,
+		str::string_ &Type,
+		str::string_ &Parameters,
+		str::string_ &ContentRetrievingMethod,
+		str::string_ &FocusingMethod );
+
+	void ExtractWidgetTypeAndParameters(
+		const xdhcmn::digest_ &Description,
+		str::string_ &Type,
+		str::string_ &Parameters );
+
+	void ExtractWidgetContentRetrievingMethod(
+		const xdhcmn::digest_ &Description,
+		str::string_ &Method );
+
+	void ExtractWidgetContentRetrievingMethod(
+		const str::string_ &Description,
+		str::string_ &Method );
+
+	void ExtractWidgetFocusingMethod(
+		const xdhcmn::digest_ &Description,
+		str::string_ &Method );
+
+	void ExtractWidgetFocusingMethod(
+		const str::string_ &Description,
+		str::string_ &Method );
+
+	void ExtractWidgetsTypesAndParametersSets(
+		const xdhcmn::digest_ &Descriptions,
+		str::strings_ &Ids,
+		str::strings_ &Types,
+		str::strings_ &ParametersSets );
+
+	void FillCasting(
+		const xdhcmn::digest_ &Description,
+		str::string_ &Id,
+		str::string_ &Casting );
+
+	void FillCastings(
+		const xdhcmn::digest_ &Descriptions,
+		str::strings_ &Ids,
+		str::strings_ &Castings );
+}
+
+#endif
