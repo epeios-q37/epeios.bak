d80bb02989de5ea7671bf57028a4b0beef907f51
csimon <epeios-q37@users.noreply.github.com>
2016-01-18 16:18:56 +0100

    18/01/2016 16:18:55

---
+++ apps/orgnzq/README.txt
@@ -0,0 +1,18 @@
+'orgnzq' is ... based on the Epeios framework and written by Claude SIMON (http://q37.info/contact/).
+
+'orgnzq' comes with different components. Each component has its own copy of the Epeios framework in
+'src/epeios' (not the entire framework ; only the modules it needs). Also each component comes which
+is own documentation, in the 'doc' subdirectory.
+
+The 'tools' directory contains the general tools (i.e. not dedicated to 'orgnzq') needed
+to launch 'orgnzq'.
+The 'frontend' directory contains the frontend part (user interface) of 'orgnzq'.
+The 'processing' directory with the processing tools and the backend of 'orgnzq'.
+
+There is another 'CHANGELOG' file in 'src/epeios' directories, which concerns the Epeios framework.
+As the application is based on the Epeios framework, some entries may also concern this appkcation.
+
+The HTML documentation files are from wiki at http://q37.info/, which content is
+published under CC BY-SA 3.0 (http://creativecommons.org/licenses/by-sa/3.0/).
+
+'orgnzq' home page :  http://q37.info/computing/epeios/apps/orgnzq/.
---
+++ apps/orgnzq/ogzinf.h
@@ -0,0 +1,44 @@
+/*
+	Copyright (C) 2015-2016 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of 'orgnzq' software.
+	
+    'orgnzq' is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    'orgnzq' is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with 'orgnzq'.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+*/
+
+// $Id$
+
+// eOrGaniZer INFormations.
+
+#ifndef OGZINF__INC
+# define OGZINF__INC
+
+# define OGZINF_LC_AFFIX	"orgnzq"
+# define OGZINF_UC_AFFIX	"ORGNZQ"
+# define OGZINF_MC_AFFIX	"orgnzq"
+# define OGZINF_UC_SHORT	"OGZ"
+# define OGZINF_LC_SHORT	"ogz"
+
+# define OGZINF_WEBSITE_URL	"http://q37.info"
+
+# define OGZINF_SOFTWARE_NAME		OGZINF_MC_AFFIX	// Commercial name of the software ; set to appropriate value if needed.
+# define OGZINF_SOFTWARE_VERSION 	"1.0"	// Version of the whole software, not of a particular program.
+# define OGZINF_SOFTWARE_URL		OGZINF_WEBSITE_URL
+
+# define OGZINF_AUTHOR_NAME		"Claude SIMON"
+# define OGZINF_AUTHOR_CONTACT	"http://q37.info/contact/"
+# define OGZINF_OWNER_NAME		"Claude SIMON"
+# define OGZINF_OWNER_CONTACT		"http://q37.info/contact/"
+
+#endif
