f1a7b9fdb24bd7ebd7f3edb13fce6b79c8b1fe46
csimon <epeios-q37@users.noreply.github.com>
2016-03-27 11:33:05 +0200

    27/03/2016 11:33:05

---
+++ stable/csdcmn.cpp
@@ -0,0 +1,80 @@
+/*
+	Copyright (C) 1999-2016 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of the Epeios framework.
+
+	The Epeios framework is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	The Epeios framework is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#define CSDCMN__COMPILATION
+
+#include "csdcmn.h"
+
+#include "str.h"
+
+using namespace csdcmn;
+
+namespace {
+	void Write_(
+		const char *Text,
+		flw::oflow__ &Flow )
+	{
+		Flow.Write( Text, strlen( Text ) + 1 );	// '+1' to put the final 0.
+	}
+}
+
+void csdcmn::SendProtocol(
+	const char *Label,
+	sVersion Version,
+	flw::oflow__ &Flow )
+{
+	bso::buffer__ Buffer;
+
+	if ( Version == UndefinedVersion )
+		qRFwk();
+
+	Write_( Label, Flow );
+
+	Write_(bso::Convert( Version, Buffer ), Flow );
+}
+
+sVersion csdcmn::GetProtocolVersion(
+	const char *Label,
+	flw::iflow__ &Flow )
+{
+	sVersion Version = UndefinedVersion;
+qRH
+	str::wString Incoming;
+	sdr::sRow Error = qNIL;
+qRB
+	Incoming.Init();
+	GetString( Flow, Incoming );
+
+	if ( Incoming != Label )
+		qRReturn;
+
+	Incoming.Init();
+	GetString( Flow, Incoming );
+
+	Incoming.ToNumber( Version, &Error );
+
+	if ( Error != qNIL )
+		Version = UndefinedVersion;
+qRR
+qRT
+qRE
+	return Version;
+}
+
+
---
+++ stable/csdcmn.h
@@ -0,0 +1,82 @@
+/*
+	Copyright (C) 1999-2016 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of the Epeios framework.
+
+	The Epeios framework is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	The Epeios framework is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+// Client-Server Devices CoMmoN
+
+#ifndef CSDCMN__INC
+# define CSDCMN__INC
+
+# define CSDCMN_NAME		"CSDCMN"
+
+# if defined( E_DEBUG ) && !defined( CSDCMN_NODBG )
+#  define CSDCMN_DBG
+# endif
+
+# include "err.h"
+
+# include "bso.h"
+# include "tol.h"
+# include "flw.h"
+# include "str.h"
+
+namespace csdcmn {
+	typedef bso::uint__ sVersion;
+
+	qCDEF( sVersion, UndefinedVersion, BSO_UINT_MAX );
+
+	void SendProtocol(
+		const char *Id,
+		sVersion Version,
+		flw::oflow__ &Flow );
+
+	sVersion GetProtocolVersion(
+		const char *Id,
+		flw::iflow__ &Flow );	// If 'UndefinedVersion' is returned, then the protocol is not the good one, or the content of the flow is not valid.
+
+	// Facilities.
+
+	inline const str::dString &GetString(
+		flw::sIFlow &Flow,
+		str::dString &Value )
+	{
+		int C = 0;
+
+		while ( ( C = Flow.Get() ) != 0 )
+			Value.Append( C );
+
+		return Value;
+	}
+
+	inline void PutString(
+		const str::dString &Value,
+		flw::sOFlow &Flow )
+	{
+		sdr::sRow Row = Value.First();
+
+		while ( Row != qNIL ) {
+			Flow.Put(Value( Row ) );
+
+			Row = Value.Next( Row );
+		}
+
+		Flow.Put( 0 );
+	}
+}
+
+#endif
