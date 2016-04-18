dfe03d14ce6cb4f4c61baa777d8ad418fe702898
csimon <epeios-q37@users.noreply.github.com>
2016-04-18 11:50:34 +0200

    18/04/2016 11:50:30

--- devel/xtf/xtf.h
+++ devel/xtf/xtf.h
@@ -17,36 +17,16 @@
 	along with the Epeios framework.  If not, see <http://www.gnu.org/licenses/>
 */
 
-//	$Id: xtf.h,v 1.42 2013/07/18 19:46:26 csimon Exp $
+// eXtended Text Flow. Text flow with extended features.
 
 #ifndef XTF__INC
-#define XTF__INC
+# define XTF__INC
 
-#define XTF_NAME		"XTF"
+# define XTF_NAME		"XTF"
 
-#define	XTF_VERSION	"$Revision: 1.42 $"
-
-#define XTF_OWNER		"Claude SIMON (http://zeusw.org/intl/contact.html)"
-
-#if defined( E_DEBUG ) && !defined( XTF_NODBG )
-#define XTF_DBG
-#endif
-
-/* Begin of automatic documentation generation part. */
-
-//V $Revision: 1.42 $
-//C Claude SIMON (http://zeusw.org/intl/contact.html)
-//R $Date: 2013/07/18 19:46:26 $
-
-/* End of automatic documentation generation part. */
-
-/* Addendum to the automatic documentation generation part. */
-//D eXtended Text Flow. 
-/* End addendum to automatic documentation generation part. */
-
-/*$BEGIN$*/
-
-//D eXtended Text Flow. Text flow with extended features.
+# if defined( E_DEBUG ) && !defined( XTF_NODBG )
+#  define XTF_DBG
+# endif
 
 # include "err.h"
 # include "bso.h"
@@ -61,6 +41,10 @@
 namespace lcl {
 	class meaning_;
 };
+
+/**************/
+/**** OLD *****/
+/**************/
 
 namespace xtf {
 	//t type of position in a text (line or column).
@@ -532,5 +516,13 @@ namespace xtf {
 	};
 }
 
-/*$END$*/
+/**************/
+/**** NEW *****/
+/**************/
+
+namespace xtf {
+	typedef extended_text_iflow__ sIFlow;
+}
+
+
 #endif
