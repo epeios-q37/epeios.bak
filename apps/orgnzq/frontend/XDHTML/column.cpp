686d173e65a0d68f34141fe6402c40d937749bbc
csimon <epeios-q37@users.noreply.github.com>
2016-05-18 10:43:07 +0200

    18/05/2016 10:43:07

---
+++ apps/orgnzq/frontend/XDHTML/column.cpp
@@ -0,0 +1,133 @@
+/*
+	Copyright (C) 2015-2016 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of 'orgnzq' software.
+
+    'orgnzq' is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    'orgnzq' is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with 'orgnzq'.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "column.h"
+
+#include "core.h"
+#include "registry.h"
+#include "sclfrntnd.h"
+
+namespace {
+
+	E_CDEF( char *, XSLAffix_, "Column" );
+
+	void GetContext_(
+		core::rSession &Session,
+		str::string_ &XML )
+	{
+	qRH
+		base::rContextRack Rack;
+	qRB
+		Rack.Init( XSLAffix_, XML, Session );
+	qRR
+	qRT
+	qRE
+	}
+
+	void SetCasting_(
+		const char *Id,
+		core::rSession &Session )
+	{
+	qRH
+		str::string XML, XSL;
+	qRB
+		XML.Init();
+		GetContext_( Session,  XML );
+
+		XSL.Init();
+		sclxdhtml::LoadXSLAndTranslateTags(rgstry::tentry___( registry::definition::XSLCastingFile, XSLAffix_ ), Session.Registry() , XSL );
+
+		Session.FillElementCastings( Id, XML, XSL );
+	qRR
+	qRT
+	qRE
+	}
+
+	static void GetContent_(
+		const sclrgstry::registry_ &Registry,
+		core::rSession &Session,
+		str::string_ &XML )
+	{
+	qRH
+		base::content_rack___ Rack;
+	qRB
+		Rack.Init( XSLAffix_, XML, Session );
+
+		Session.User.DumpColumnBuffer(Rack() );
+	qRR
+	qRT
+	qRE
+	}
+}
+
+void column::SetLayout(
+	const char *Id,
+	core::rSession &Session )
+{
+qRH
+	str::string XML, XSL;
+qRB
+	XML.Init(); 
+	GetContent_( Session.Registry(), Session, XML );
+
+	XSL.Init();
+	sclxdhtml::LoadXSLAndTranslateTags( rgstry::tentry___( registry::definition::XSLLayoutFile, XSLAffix_ ), Session.Registry(), XSL );
+
+	Session.FillElement( Id, XML, XSL );
+
+	SetCasting_( Id, Session );
+
+//	Session.SwitchTo( core::fColumn );
+qRR
+qRT
+qRE
+}
+
+BASE_AC( column::sCreateField )
+{
+qRH
+	str::wString Label, Comment;
+	frdinstc::sType Type = frdinstc::UndefinedType;
+	frdinstc::sNumber Number = frdinstc::UndefinedNumber;
+qRB
+	Label.Init();
+	Session.GetContent( "FieldLabel", Label );
+
+	Comment.Init();
+	Session.GetContent( "FieldComment", Comment );
+
+	if ( !Session.GetNumericalProperty( "Type", "value", **Type ) )
+		sclmisc::ReportAndAbort( "FieldTypeIsRequired" );
+
+	if ( !Session.GetNumericalProperty( "Number", "value", **Number ) )
+		sclmisc::ReportAndAbort( "FieldNumberIsRequired" );
+
+	Label.StripCharacter( ' ' );
+	
+	if ( Label.Amount() == 0 )
+		sclmisc::ReportAndAbort( "FieldLabelCanNotBeEmpty" );
+
+	Session.User.CreateField( Type, Number, Label, Comment );
+
+	main::SetRecordLayout( Session );
+qRR
+qRT
+qRE
+}
+
---
+++ apps/orgnzq/frontend/XDHTML/column.h
@@ -0,0 +1,50 @@
+/*
+	Copyright (C) 2015-2016 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of 'orgnzq' software.
+
+    'orgnzq' is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    'orgnzq' is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with 'orgnzq'.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Column.
+
+#ifndef COLUMN__INC
+# define COLUMN__INC
+
+# include "base.h"
+
+namespace column {
+	BASE_ACD( CreateField );
+
+	class fActionCallbacks
+	{
+	public:
+		BASE_ACU( CreateField );
+		void reset( bso::bool__ P = true )
+		{
+			BASE_ACR( CreateField );
+		}
+		qCVDTOR( fActionCallbacks );
+		void Init( void )
+		{
+			BASE_ACI( CreateField );
+		}
+	};
+
+	void SetLayout(
+		const char *Id,
+		core::rSession &Session );
+}
+
+#endif
