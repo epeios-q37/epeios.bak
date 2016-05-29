f1eafbabb347090aa7c79176faa7e025e74d15fe
csimon <epeios-q37@users.noreply.github.com>
2016-05-29 16:00:19 +0200

    29/05/2016 16:00:17

--- devel/sclxdhtml/sclxdhtml.cpp
+++ devel/sclxdhtml/sclxdhtml.cpp
@@ -173,7 +173,7 @@ namespace {
 		XSL.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\
 			<xsl:stylesheet version=\"1.0\"\
 			                xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">\
-				<xsl:output method=\"text\"\
+				<xsl:output method=\"html\"\
 					        encoding=\"utf-8\"/>\
 				<xsl:template match=\"/\">\
 					<xsl:value-of select=\"Content\"/>\
