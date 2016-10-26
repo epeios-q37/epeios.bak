1551e4ad99bcd41b0a734e2b7a174c25a792fdf0
csimon <epeios-q37@users.noreply.github.com>
2016-10-26 19:03:47 +0200

    26/10/2016 19:03:46

---
+++ apps/mmuaq/muainf.h
@@ -0,0 +1,42 @@
+/*
+	'muainf.h' by Claude SIMON (http://q37.info/contact/)
+
+    This file is part of 'MMUAq' software.
+	
+    'MMUAq' is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    'MMUAq' is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 'MMUAq'.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+*/
+
+// MUA INFormations.
+
+#ifndef MUAINF__INC
+# define MUAINF__INC
+
+# define MUAINF_LC_AFFIX	"mmuaq"
+# define MUAINF_UC_AFFIX	"MMUAQ"
+# define MUAINF_MC_AFFIX	"MMUAq"
+# define MUAINF_UC_SHORT	"MUA"
+# define MUAINF_LC_SHORT	"mua"
+
+# define MUAINF_WEBSITE_URL	"http://q37.info"
+
+# define MUAINF_SOFTWARE_NAME		MUAINF_MC_AFFIX	// Commercial name of the software ; set to appropriate value if needed.
+# define MUAINF_SOFTWARE_VERSION 	"1.0"	// Version of the whole software, not of a particular program.
+# define MUAINF_SOFTWARE_URL		MUAINF_WEBSITE_URL
+
+# define MUAINF_AUTHOR_NAME		"Claude SIMON"
+# define MUAINF_AUTHOR_CONTACT	"http://q37.info/contact/"
+# define MUAINF_OWNER_NAME		"Claude SIMON"
+# define MUAINF_OWNER_CONTACT		"http://q37.info/contact/"
+
+#endif
