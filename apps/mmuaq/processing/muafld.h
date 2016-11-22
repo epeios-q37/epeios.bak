1cf7236d7c1a7beed73f0bbd445460de02957b6e
csimon <epeios-q37@users.noreply.github.com>
2016-11-22 12:52:41 +0100

    22/11/2016 12:52:40

---
+++ apps/mmuaq/processing/muafld.h
@@ -0,0 +1,182 @@
+/*
+	Copyright (C) 2016 Claude SIMON (http://zeusw.org/epeios/contact.html).
+
+	This file is part of 'MMUAq' software.
+
+    'MMUAq' is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    'MMUAq' is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with 'MMUAq'.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// MUA FoLDer
+
+#ifndef MUAFLD__INC
+# define MUAFLD__INC
+
+# ifdef XXX_DBG
+#	define MUAFLD__DBG
+# endif
+
+# include "muabsc.h"
+# include "muamel.h"
+
+# include "dtr.h"
+# include "lstcrt.h"
+
+namespace muafld {
+	class dFolder {
+	public:
+		struct s {
+			str::dString::s Name;
+			muamel::dRows::s Mails;
+		};
+		str::dString Name;
+		muamel::dRows Mails;
+		dFolder( s &S )
+		: Name( S.Name ),
+		  Mails( S.Mails )
+		{}
+		void reset( bso::sBool P = true )
+		{
+			tol::reset( P, Name, Mails );
+		}
+		void plug( qASd *AS )
+		{
+			tol::plug( AS, Name, Mails );
+		}
+		dFolder &operator =(const dFolder &F)
+		{
+			Name = F.Name;
+			Mails = F.Mails;
+
+			return *this;
+		}
+		void Init( const str::dString &Name )
+		{
+			this->Name.Init( Name );
+			tol::Init( Mails );
+		}
+		void Add( muamel::sRow Mail )
+		{
+			Mails.Add( Mail );
+		}
+	};
+
+	qW( Folder );
+
+	qROW( Row );
+	qROWS( Row );
+
+	typedef lstcrt::qLCRATEd( dFolder, sRow ) dFolders_;
+	qW( Folders_ );
+
+	typedef dtr::qDTREEd( sRow ) dTree_;
+	qW( Tree_ );
+
+	class dFoldersTree
+	: public dTree_
+	{
+	private:
+		sRow Normalize_( muafld::sRow Row ) const
+		{
+			if ( Row == qNIL )
+				Row = S_.Root;
+
+			if ( !Folders.Exists( Row ) )
+				qRGnr();
+
+			return Row;
+		}
+	public:
+		struct s
+		: public dTree_::s
+		{
+			dFolders_::s Folders;
+			sRow Root;
+		} &S_;
+		dFolders_ Folders;
+		dFoldersTree( s &S )
+		: S_( S ),
+		  dTree_( S ),
+		  Folders( S.Folders )
+		{}
+		void reset( bso::sBool P = true )
+		{
+			dTree_::reset( P );
+			tol::reset( P, Folders );
+
+			S_.Root = qNIL;
+		}
+		void plug( qASd *AS )
+		{
+			dTree_::plug( AS );
+			tol::plug( AS, Folders );
+		}
+		dFoldersTree &operator =(const dFoldersTree &FD)
+		{
+			dTree_::operator =( FD );
+			Folders = FD.Folders;
+			S_.Root = FD.S_.Root;
+
+			return *this;
+		}
+		void Init( void )
+		{
+			dTree_::Init();
+			tol::Init( Folders );
+
+			S_.Root = Folders.New();
+			Folders( S_.Root ).Init( str::wString() );
+			dTree_::Allocate( 1 );
+		}
+		const str::dString &GetName(
+			sRow Row,
+			str::dString &Name ) const
+		{
+			Row = Normalize_( Row );
+			Name.Append( Folders( Row ).Name );
+
+			return Name;
+		}
+		const dRows &GetFolders(
+			sRow Row,
+			dRows &Rows ) const;
+		void Add(
+			sRow Folder,
+			muamel::sRow Mail )
+		{
+			Folder = Normalize_( Folder );
+
+			Folders( Folder ).Add( Mail );
+			Folders.Flush();
+		}
+		sRow CreateChild(
+			const str::dString &Name,
+			sRow Parent )
+		{
+			sRow Row = Folders.New();
+
+			Parent = Normalize_( Parent );
+
+			Folders(Row).Init( Name );
+
+			dTree_::Allocate(Folders.Amount() );
+
+			BecomeLastChild( Row, Parent );
+
+			return Row;
+		}
+	};
+}
+
+
+#endif
