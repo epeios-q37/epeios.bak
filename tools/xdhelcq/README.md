d700f500eaddfaf0bea7ab6fdcdad72eb10d89df
csimon <epeios-q37@users.noreply.github.com>
2017-11-04 19:45:19 +0100

    04/11/2017 19:45:13

---
+++ tools/xdhelcq/README.md
@@ -0,0 +1,21 @@
+# XDHELCq
+
+*XDHTML* module wrapper for *electron*
+
+For more information, see [http://q37.info/tools/xdhelcq/](http://q37.info/tools/xdhelcq/).
+
+[![NPM](https://nodei.co/npm/xdhelcq.png)](https://nodei.co/npm/xdhelcq/)
+
+*GNU/Linux* & *OS X* : [![Travis CI](https://travis-ci.org/epeios-q37/xdhelcq.png)](https://travis-ci.org/epeios-q37/xdhelcq)
+ 
+*Windows* : [![AppVeyor](http://ci.appveyor.com/api/projects/status/github/epeios-q37/xdhelcq)](http://ci.appveyor.com/project/epeios-q37/xdhelcq)
+
+
+## Note for the developer and maintainer of this wrapper.
+
+Handle:
+ 
+  * <https://electron.atom.io/docs/tutorial/using-native-node-modules/>
+  * <https://github.com/juliangruber/require-rebuild>
+
+ 
---
+++ tools/xdhelcq/xdhelcq.cpp
@@ -0,0 +1,276 @@
+/*
+	Copyright (C) 2017 by Claude SIMON (http://zeusw.org/epeios/contact.html).
+
+	This file is part of XDHELCq.
+
+    XDHELCq is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XDHELCq is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with XDHELCq.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "xdhelcq.h"
+
+#include "registry.h"
+
+#include "nodeq.h"
+
+#include "sclerror.h"
+#include "sclargmnt.h"
+#include "sclmisc.h"
+
+#include "dir.h"
+#include "err.h"
+#include "cio.h"
+#include "epsmsc.h"
+#include "xpp.h"
+#include "fnm.h"
+#include "flf.h"
+
+using cio::CErr;
+using cio::COut;
+using cio::CIn;
+
+namespace {
+	namespace {
+		void GetWrapperInfo_( str::dString &Info )
+		{
+		qRH
+			flx::rStringOFlow BaseFlow;
+			txf::sOFlow Flow;
+		qRB
+			BaseFlow.Init( Info );
+			Flow.Init( BaseFlow );
+
+			Flow << NAME_MC << " v" << VERSION << " - Node v" NODE_VERSION_STRING " ABI v" NODE_STRINGIFY( NODE_MODULE_VERSION ) << txf::nl
+			<< txf::pad << "Build : " __DATE__ " " __TIME__ " (" << cpe::GetDescription() << ')';
+		qRR
+		qRT
+		qRE
+		}
+	}
+
+	void GetWrapperInfo_( const v8::FunctionCallbackInfo<v8::Value>& Args )
+	{
+	qRH
+		str::wString Info;
+		v8q::sLString String;
+	qRB
+		Info.Init();
+
+		GetWrapperInfo_( Info );
+
+		String.Init( Info );
+
+		Args.GetReturnValue().Set( String.Core() );
+	qRR
+	qRT
+	qRE
+	}
+
+	void GetModuleInfo_( const v8::FunctionCallbackInfo<v8::Value>& Args )
+	{
+	qRH
+		str::wString Info;
+		v8q::sLString String;
+	qRB
+		Info.Init();
+
+//		if ( !wrapper::GetLauncherInfo( Info ) )
+			sclmisc::GetBaseTranslation( "NoRegisteredComponent", Info );
+
+		String.Init( Info );
+
+		Args.GetReturnValue().Set( String.Core() );
+	qRR
+	qRT
+	qRE
+	}
+}
+
+namespace {
+
+	void InitWithModuleFilename_(
+		v8::Local<v8::Value> Module,
+		v8q::sLString &Filename )
+	{
+		nodeq::sLObject Object;
+
+		Object.Init( Module );
+
+		Filename.Init( Object.Get( "filename" ) );
+	}
+
+	void GetModuleFilename_(
+		v8::Local<v8::Value> Module,
+		str::dString &Filename )
+	{
+	qRH
+		v8q::sLString V8Filename;
+	qRB
+		InitWithModuleFilename_( Module, V8Filename );
+
+		V8Filename.Get( Filename );
+	qRR
+	qRT
+	qRE
+	}
+#if 0
+	void GetParentModuleFilename_(
+		v8::Local<v8::Value> Module,
+		str::dString &Filename )
+	{
+		nodeq::sLObject Object;
+
+		Object.Init( Module );
+
+		GetModuleFilename_( Object.Get( "parent" ), Filename );
+	}
+#endif
+	// Returns true if 'Filename' ends with '.node'.
+	bso::sBool IsNJSq_( const fnm::rName &Filename )
+	{
+		bso::sBool Is = false;
+	qRH
+		fnm::rName Basename;
+		str::wString Buffer;
+	qRB
+		Basename.Init();
+		fnm::GetBasename( Filename, Basename );
+
+		Buffer.Init();
+		Basename.UTF8( Buffer );
+
+		Is = Buffer == NAME_LC ".node";
+	qRR
+	qRT
+	qRE
+		return Is;
+	}
+	void GetAddonFilename_(
+		v8::Local<v8::Value> Module,
+		str::dString &Filename )
+	{
+		Filename.Init();
+		GetModuleFilename_( Module, Filename );
+
+		if ( !IsNJSq_( Filename ) )
+			qRGnr();
+	}
+}
+
+void GetAddonLocation_(
+	v8::Local<v8::Value> Module,
+	str::dString &Location )
+{
+qRH
+	str::wString Filename;
+	fnm::rName Path;
+qRB
+	Filename.Init();
+
+	GetAddonFilename_( Module, Filename );
+
+	Path.Init();
+	fnm::GetLocation( Filename, Path );
+
+	Path.UTF8( Location );
+qRR
+qRT
+qRE
+}
+
+namespace {
+	void OnExit_( void *UP )
+	{
+		qRVct();
+//		wrapper::DeleteLauncher();
+	}
+}
+
+namespace {
+	err::err___ qRRor_;
+	sclerror::rError SCLError_;
+	scllocale::rRack Locale_;
+	sclmisc::sRack Rack_;
+}
+
+namespace {
+	void ErrFinal_( v8::Isolate *Isolate = NULL )
+	{
+	qRH
+		str::wString Message;
+		err::buffer__ Buffer;
+	qRB
+		Isolate = v8q::GetIsolate( Isolate );
+
+		Message.Init();
+
+		if ( ERRType != err::t_Abort ) {
+			Message.Append( err::Message( Buffer ) );
+
+			ERRRst();	// To avoid relaunching of current error by objects of the 'FLW' library.
+		} else if ( sclerror::IsErrorPending() )
+			sclmisc::GetSCLBasePendingErrorTranslation( Message );
+
+		if ( Isolate != NULL )
+			Isolate->ThrowException( v8::Exception::Error( v8q::ToString( Message ) ) );
+		else
+			cio::CErr << txf::nl << Message << txf::nl;
+	qRR
+		ERRRst();
+	qRT
+	qRE
+	}
+}
+
+void Start(
+	v8::Local<v8::Object> Exports,
+	v8::Local<v8::Value> Module,
+	void* priv )
+{
+qRFH
+	str::wString Location;
+	str::wString Filename;
+qRFB
+	NODE_SET_METHOD( Exports, "wrapperInfo", GetWrapperInfo_ );
+	NODE_SET_METHOD( Exports, "moduleInfo", GetModuleInfo_ );
+/*
+	cio::Initialize( cio::GetConsoleSet() );
+
+	qRRor_.Init();
+	SCLError_.Init();
+	Locale_.Init();
+
+	Rack_.Init( qRRor_, SCLError_, cio::GetSet( cio::t_Default ), Locale_ );
+
+	Location.Init();
+	GetAddonLocation_( Module, Location );
+
+	sclmisc::Initialize( Rack_, Location );
+
+	node::AtExit( OnExit_, NULL );
+	*/
+	/*
+	error_::Initialize();
+
+	error_::Launch();
+	*/
+qRFR
+qRFT
+qRFE( ErrFinal_() )
+}
+
+NODE_MODULE( xdhelcq, Start );
+
+const char *sclmisc::SCLMISCTargetName = NAME_LC;
+const char *sclmisc::SCLMISCProductName = NAME_MC;
+
