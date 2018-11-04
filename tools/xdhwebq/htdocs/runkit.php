a3dc346d541ae142c4569e4ddcf70d6e22ae96d9
csimon <epeios-q37@users.noreply.github.com>
2018-11-04 08:25:04 +0100

    04/11/2018 08:24:52

--- tools/xdhwebq/htdocs/runkit.php
+++ tools/xdhwebq/htdocs/runkit.php
@@ -1,27 +1,27 @@
 <?php
 /*
-	Copyright (C) 2018 Claude SIMON (http://q37.info/contact/).
+Copyright (C) 2018 Claude SIMON (http://q37.info/contact/).
 
-	This file is part of xdhwebq.
+This file is part of xdhwebq.
 
-	xdhwebq is free software: you can redistribute it and/or
-	modify it under the terms of the GNU Affero General Public License as
-	published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
+xdhwebq is free software: you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-	xdhwebq is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-	Affero General Public License for more details.
+xdhwebq is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Affero General Public License for more details.
 
-	You should have received a copy of the GNU Affero General Public License
-	along with xdhwebq. If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License
+along with xdhwebq. If not, see <http://www.gnu.org/licenses/>.
  */
 
- /*
-	Gets a JS file (for Node.js) from the 'asset' directory on 'q37.info',
-	and creates a page which embeds it on RunKit.
-*/
+/*
+Gets a JS file (for Node.js) from the 'assets' directory on 'q37.info',
+and creates a page which embeds it on RunKit.
+ */
 
 $source = <<<EOT
 /*
@@ -37,29 +37,25 @@ $source = <<<EOT
 	The program will last only a few tens of seconds,
 	due to a RunKit timeout.
 
-	Feel free to modify the source code.
-	
+	You can modify the source code for your own testings.
+
 	Enjoy !
 */
 
-
 EOT;
+
 $app = $_GET['app'];
 
-$token = array_key_exists( 'token', $_GET ) ? $_GET['token'] : uniqid();
+$token = array_key_exists('token', $_GET) ? $_GET['token'] : uniqid();
 
-$source = str_replace( "$NAME$", $app, $source );
+$source = str_replace("$NAME$", $app, $source);
 
-// $source .= "\n\nprocess.env.ATK_TOKEN=\"" . $token . "\";\n\n";
+$source .= file_get_contents("http://q37.info/download/assets/" . $app . ".js");
 
-$source .= file_get_contents( "http://q37.info/download/assets/" . $app . ".js" );
-
-$source .=<<<EOT
-
+$source .= <<<EOT
 
 console.log("Scroll down after a little while, as new things will be displayed.");
 console.log("Below URL should have been automatically opened in your web browser.");
-console.log("If not, pop up windows are probably not allowed by your web browser.");
 "NOTA: the program will be stopped after a while due to RunKit timeout."
 
 EOT;
@@ -83,16 +79,19 @@ $page = '<!DOCTYPE html>
 		<script>
 function onLoad( notebook )
 {
-    alert("The \'' . $app . '\' application will now be launched on RunKit and be automatically opened in your web browser.\nIf not, follow the instructions at the bottom of the RunKit page.\n\nRemember: the application will be stopped after a few tens of seconds due to RunKit timeout!");
-    notebook.evaluate();
+    if ( confirm("The \'' . $app . '\' application will now be launched on RunKit and be automatically opened in your web browser.\n\nThe application will become unresponsive after a few tens of seconds due to RunKit timeout!\nYou can then simply go back to the RunKit page.") )
+					notebook.evaluate();
+				else
+				 window.scrollTo(0,0);
+
 }
 function onEvaluate()
 {
 	window.scrollTo(0,document.body.scrollHeight);
     let url = "' . $url . '";
 	setTimeout( () => {
-		window.open( url );
-	}, 2000 );
+		window.open( url, "_self" );
+	}, 5000 );
 }
 var source = "' . $esource . '";
 var notebook = RunKit.createNotebook({
@@ -108,17 +107,17 @@ env: [ "ATK_TOKEN=' . $token . '" ]
 	</body>
 </html>';
 
-
 // Microsoft Edge does have problems when the source is too long !
-if ( preg_match('/Edge/i', $_SERVER['HTTP_USER_AGENT']) == 1 )
-	$page = '<h3>Applications based on the <a href="http://atlastk.org"><em>Atlas</em> toolkit</a> will work on all modern web browser, including Microsoft Edge. But, due to limitations (bugs?) of Microsoft Edge, it can not be used to launch the online demos. You have to switch to another web navigator for this.</h3>';
+if (preg_match('/Edge/i', $_SERVER['HTTP_USER_AGENT']) == 1) {
+ $page = '<h3>Applications based on the <a href="http://atlastk.org"><em>Atlas</em> toolkit</a> will work on all modern web browser, including Microsoft Edge. But, due to limitations (bugs?) of Microsoft Edge, it can not be used to launch the online demos. You have to switch to another web navigator for this.</h3>';
+}
 
 $page2 = '
 <!DOCTYPE html>
 <html>
 	<head>
 		<script>
-var url = "data:text/html;base64,' . base64_encode( $page ) . '";
+var url = "data:text/html;base64,' . base64_encode($page) . '";
 alert( url );
 window.open( url );
 		</script>
@@ -141,7 +140,7 @@ $page3 = '
 </head>
 <body>
 <div id="script">
-' . htmlspecialchars( $source ) . '
+' . htmlspecialchars($source) . '
 </div>
 </body>
 </html>
@@ -170,5 +169,4 @@ onLoad: (notebook) => {notebook.evaluate()},
 </html>';
 
 echo $page;
-
 ?>
