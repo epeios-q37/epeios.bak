0837cbaa734cc349a73a503ad37cdc1e70c55a60
csimon <epeios-q37@users.noreply.github.com>
2020-02-28 17:05:21 +0100

    28/02/2020 17:05:12

---
+++ tools/wsq/CLI/README.md
@@ -0,0 +1,10 @@
+# wsq-cli
+... with command-line interface.
+
+Go to [http://q37.info/tools/wsq/](http://q37.info/s/t/wsq/) for more information.
+
+## Status
+*GNU/Linux* & *OS X* : [![Travis CI](https://travis-ci.org/epeios-q37/wsq-cli.png)](https://travis-ci.org/epeios-q37/wsq-cli)
+ 
+*Windows* : [![AppVeyor](http://ci.appveyor.com/api/projects/status/github/epeios-q37/wsq-cli)](http://ci.appveyor.com/project/epeios-q37/wsq-cli)
+
---
+++ tools/wsq/CLI/wsq.h
@@ -0,0 +1,39 @@
+/*
+	Copyright (C) 2019 Claude SIMON (http://q37.info/contact/).
+
+	This file is part of the 'WSq' tool.
+
+    'WSq' is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    'WSq' is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with 'WSq'.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef WSQ_INC_
+# define WSQ_INC_
+
+# include "scli.h"
+
+# define NAME_MC			"WSq"
+# define NAME_UC			"WSQ"
+# define NAME_LC			"wsq"
+# define WEBSITE_URL		"http://q37.info/tools/wsq/"
+# define AUTHOR_NAME		"Claude SIMON"
+# define AUTHOR_CONTACT		"http://q37.info/contact/"
+# define OWNER_NAME			"Claude SIMON"
+# define OWNER_CONTACT		"http://q37.info/contact/"
+# define COPYRIGHT			COPYRIGHT_YEARS " " OWNER_NAME " (" OWNER_CONTACT ")"
+
+SCLI_DEC( wsq )
+
+#endif
+
