7b5421cf047b343216c52bb26f53678b0e78b359
csimon <epeios-q37@users.noreply.github.com>
2021-06-09 12:37:09 +0200

    09/06/2021 12:37:02

--- tools/xdhq/examples/PYH/ReversiTuto/main.py
+++ tools/xdhq/examples/PYH/ReversiTuto/main.py
@@ -41,6 +41,7 @@ TOKEN = {
 }
 
 board = None
+available = None
 turn = None
 
 def has_my_piece(bw, x, y, delta_x, delta_y):
@@ -76,12 +77,30 @@ def reversible_directions(bw, x, y):
       directions.append(d)
   return directions
 
+def reverse_piece(bw, x, y, delta_x, delta_y):
+  global board
+  "Reverse pieces in the direction of (delta_x, delta_y) from (x, y) until bw."
+  assert bw in (BLACK, WHITE)
+
+  x += delta_x
+  y += delta_y
+  assert board[x][y] in (BLACK, WHITE)
+
+  if board[x][y] == bw:
+    return
+
+  board[x][y] = bw
+  return reverse_piece(bw, x, y, delta_x, delta_y)  
+
 def is_allowed(x, y, bw):
-  return len(reversible_directions(bw, x, y)) != 0
+  if bw == EMPTY:
+    return False
+  else:
+    return len(reversible_directions(bw, x, y)) != 0
 
 
 def init_board():
-  global board
+  global board, available, turn
 
   board = []
 
@@ -91,23 +110,34 @@ def init_board():
   board[3][3] = board[4][4] = BLACK
   board[4][3] = board[3][4] = WHITE
 
+  available = True
+  turn = BLACK
+
 
 class Reversi:
   def __init__(self):
-    self.bw = EMPTY
+    self.bw = None
 
   def init(self, bw):
     self.bw = bw
 
-def draw_board(reversi, dom, playability = True):
+def set_status(dom, status, color = "black"):
+  dom.inner("Status",f'<span style="color: {color}">{status}</span>')    
+
+def draw_board(reversi, dom):
   tokens = {}
   toAdd = {}
   toRemove = {}
 
-  for y, row in enumerate(board):
-    for x, r in enumerate(row):
+  bw = reversi.bw
+
+  if bw == None:
+    bw = turn
+
+  for x, row in enumerate(board):
+    for y, r in enumerate(row):
       tokens[f'{x}{y}'] = TOKEN[r]
-      playable = playability and (r == EMPTY) and (is_allowed(y, x, reversi.bw if reversi.bw != EMPTY else BLACK))
+      playable = (r == EMPTY) and bw == turn and is_allowed(x, y, bw)
 
       if ( playable ):
         toAdd[f'{x}{y}'] = "playable"
@@ -119,15 +149,81 @@ def draw_board(reversi, dom, playability = True):
   dom.removeClasses(toRemove)
 
 
+def count(bwe):
+  "Count pieces or empty spaces in the board"
+  assert bwe in (BLACK, WHITE, EMPTY)
+  n = 0
+  for i in range(8):
+    for j in range(8):
+      if board[i][j] == bwe:
+        n += 1
+  return n
+
+
+def Refresh(dom, reversi):
+  if reversi.bw == None:
+    if not available:
+      reversi.bw = EMPTY
+  draw_board(reversi, dom)
+
+  dom.set_values({
+    "black": count(BLACK),
+    "white": count(WHITE)
+  })
+
+#  return
+  if reversi.bw == EMPTY:
+    set_status(dom, f"'{TOKEN[turn]}' (you cannot play)")
+  elif reversi.bw == None:
+    if turn == BLACK:
+      set_status(dom, "Play ('X') or wait opponent's move!", "blue")
+    else:
+      set_status(dom, "First player ('X') played!", "blue")
+  elif reversi.bw == turn:
+    set_status(dom, f"Your turn ('{TOKEN[turn]}')!", "green")
+  else:
+    set_status(dom, "Wait for opponent's move!", 'red"')
+
+
 def ac_connect(reversi, dom, id):
   if ( board == None ):
     init_board()
-  dom.inner("", open("TutoMain.html").read())
-  draw_board(reversi, dom)
+  if not available:
+    reversi.bw = EMPTY
+  dom.inner("", open("Main.html").read())
+  Refresh(dom, reversi)
+
+
+def ac_play(reversi, dom, id):
+  global board, turn, available
+
+  x, y = int(id[0]), int(id[1])
+
+  bw = reversi.bw
+
+  if bw == None:
+    bw = reversi.bw = turn
+    available = turn == BLACK
+
+  if ( bw != EMPTY ) and bw == turn:
+    if is_allowed(x, y, bw):
+      for delta in reversible_directions(bw, x, y):
+        reverse_piece(bw, x, y, delta[0], delta[1])
+      board[x][y] = bw
+
+      turn = turn * -1
+
+    atlastk.broadcast_action("Refresh")
+
+def ac_refresh(reversi, dom):
+  Refresh(dom, reversi)
 
 
 CALLBACKS = {
   "": ac_connect,
+  "Play": ac_play,
+  "Refresh": ac_refresh,
+  "New": 
 }
 
-atlastk.launch(CALLBACKS,  Reversi, open("TutoHead.html").read())
+atlastk.launch(CALLBACKS,  Reversi, open("Head.html").read())
