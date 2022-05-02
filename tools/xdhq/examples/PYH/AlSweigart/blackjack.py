16e78b8bc3338e313662853e7b7dc9b32c379991
csimon <epeios-q37@users.noreply.github.com>
2022-05-02 18:31:03 +0200

    02/05/2022 18:30:49

--- tools/xdhq/examples/PYH/AlSweigart/blackjack.py
+++ tools/xdhq/examples/PYH/AlSweigart/blackjack.py
@@ -23,11 +23,13 @@ def main():
 
 
 def newGame():
-  global deck, playerHand, dealerHand
+  global deck, playerHand, dealerHand, money, bet
 
   deck = getDeck()
-  dealerHand = [deck.pop(), deck.pop()]
-  playerHand = [deck.pop(), deck.pop()]
+  dealerHand = [54]
+  playerHand = [54]
+  money = 5000
+  bet = 0
 
 
 def initSVGCards():
@@ -68,6 +70,23 @@ def displayHands(dom, dealerHand, playerHand, showDealerHand):
     dom.setContent("player_hand_value", getHandValue(playerHand))
     displayCards(dom, "player_cards", playerHand)
 
+    if len(playerHand) >= 2:
+      notify(dom, "Please hit or stand.")    
+    else:
+      notify(dom, "Please bet.")
+
+
+def displayMoneyAndBet(dom, money, bet):
+  dom.inner("money", '<output class="fade-in">{}</output>'.format(money))
+  dom.inner("bet", '<output class="fade-in">{}</output>'.format(bet))
+
+  if bet == 0:
+    notify(dom, "Please make a bet!")
+  elif len(playerHand) <= 2:
+    notify(dom, "Please bet, hit or double down.")
+  else:
+    notify(dom, "Please hit or stand.")
+
 
 def getHandValue(cards):
     """Returns the value of the cards. Face cards are worth 10, aces are
@@ -102,8 +121,6 @@ def displayCards(dom, id, cards):
     for card in reversed(cards):
       html += getSVGCard(card)
 
-    html += ""
-
     dom.inner(id, html)
 
 
@@ -112,32 +129,160 @@ def notify(dom, text):
   dom.inner("output", '<output class="fade-in">{}</output>'.format(text))
 
 
+def testAction(dom, action):
+  playerHandSize = len(playerHand)
+
+  if playerHandSize > 2:
+    if action not in ['H', 'S']:
+      notify(dom, "You van only hit or stand!")
+      return False
+  elif playerHandSize == 2:
+    if action not in ['H', 'S', 'D']:
+      notify(dom, "You can only hit, stand or double down!")
+      return False
+  elif action not in ['+', '-', 'B']:
+    notify(dom, "Yu can only bet!")
+    return False
+
+  return True
+
+
 def acConnect(dom):
   dom.inner("", '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="0" height="0">' + SVG_DEFS + '</svg>' + BODY)
   displayHands(dom, dealerHand, playerHand, False)
+  displayMoneyAndBet(dom, money, bet)
+
+
+def acAdd(dom, id):
+  global money, bet
+
+  if not testAction(dom, '+'):
+    return
+
+  value = int(dom.get_mark(id))
+
+  if value > money:
+    notify(dom, "You can't bet that much!")
+    return
+
+  bet += value
+  money -= value
+
+  atlastk.broadcast_action("DisplayMoneyAndBet")
+
+
+def acSub(dom, id):
+  global money, bet
+
+  if not testAction(dom, '-'):
+    return
+
+  value = int(dom.get_mark(id))
+
+  if value > bet:
+    notify(dom, "You can't to remove that much!")
+    return
+
+  bet -= value
+  money += value
+
+  atlastk.broadcast_action("DisplayMoneyAndBet")
+
+
+def acBet(dom):
+  global dealerHand, playerHand
+
+  if not testAction(dom, 'B'):
+    return
+
+  dealerHand = [deck.pop(), deck.pop()]
+  playerHand = [deck.pop(), deck.pop()]
+  atlastk.broadcast_action("DisplayIntermediateHands")
 
 
 def acHit(dom):
+  if not testAction(dom, 'H'):
+    return
+
   playerHand.append(deck.pop())
-  atlastk.broadcast_action("Display")
+  atlastk.broadcast_action("DisplayIntermediateHands")
+
+
+def acStand(dom):
+  if not testAction(dom, 'S'):
+    return
+
+
+def acDoubleDown(dom):
+  global money, bet, playerHand
+
+  if not testAction(dom, 'D'):
+    return
+
+  if money == 0:
+    notify(dom, "Not enough money to double down!")
+    return
+
+  value = min(bet, money)
+  money -= value
+  bet += value
+
+  playerHand.append(deck.pop())
+  atlastk.broadcast_action("DisplayFinalHands")  
+  atlastk.broadcast_action("DisplayMoneyAndBet")
 
 
 CALLBACKS = {
   "": acConnect,
-  "Display": lambda dom: displayHands(dom, dealerHand, playerHand, False),
+  "DisplayIntermediateHands": lambda dom: displayHands(dom, dealerHand, playerHand, False),
+  "DisplayFinalHands": lambda dom: displayHands(dom, dealerHand, playerHand, True),
+  "DisplayMoneyAndBet": lambda dom: displayMoneyAndBet(dom, money, bet),
+  "+": acAdd,
+  "-": acSub,
+  "Bet": acBet,
   "Hit": acHit,
+  "Stand": acStand,
+  "DoubleDown": acDoubleDown
 }
+
+CHIP_VALUES = [1, 5, 10, 50, 100, 500]
+
+CHIP = """
+<span xdh:mark="{}" class="chip">
+  <button xdh:onevent="+">+</button>
+  <fieldset class="chip-value">{}</fieldset>
+  <button xdh:onevent="-">-</button>
+</span>
+"""
+
+CHIPS = """
+<div class="chips">
+""" + len(CHIP_VALUES) * CHIP + """
+  <span>
+    <fieldset>
+      <legend>Money</legend>
+      <span id="money">5000</span>
+    </fieldset>
+    <fieldset>
+      <legend>Bet</legend>
+      <span id="bet">500</span>
+    </fieldset>
+  </span>
+</div>
+"""
 
 BODY = """
 <fieldset>
-  <span style="display: flex; justify-content: space-around">
+  <div style="display: flex; justify-content: space-around; padding-bottom: 10px;">
+    <button xdh:onevent="Bet">Bet</button>
     <button xdh:onevent="Hit">Hit</button>
     <button xdh:onevent="Stand">Stand</button>
-    <button>Double down</button>
+    <button xdh:onevent="DoubleDown">Double down</button>
     <span></span>
     <button>New game</button>
-  </span>
+  </div>
   <fieldset id="output"></fieldset>
+""" + CHIPS.format(*[v for v in CHIP_VALUES for _ in range(2) ]) + """
   <span style="display: flex">
     <fieldset>
       <legend>
@@ -159,9 +304,46 @@ BODY = """
 
 HEAD = """
 <style>
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  .fade-in {
+    animation-name: fadeIn;
+    animation-duration: 1s;
+  }
   #dealer_cards, #player_cards {
     display: flex;
     flex-direction: column;
+  }
+  .chip {
+    display: flex;
+    flex-direction: column;
+  }
+  .chip-value {
+    width: 3ch;
+    justify-content: center;
+    display: flex;
+  }
+  .chips {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 5px;
+  }
+  .chips span fieldset {
+    padding: 0px;
+  }
+  .chips span fieldset span {
+    justify-content: right;
+    display: flex;
+  }
+  .chips span button {
+    font-size: large;
   }
 </style>
 """
