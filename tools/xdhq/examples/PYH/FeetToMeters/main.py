fe6e86dcc6ebed1a2007ffadba7b9b93a45303fd
csimon <epeios-q37@users.noreply.github.com>
2022-08-27 17:04:47 +0200

    27/08/2022 17:04:33

---
+++ tools/xdhq/examples/PYH/FeetToMeters/main.py
@@ -0,0 +1,62 @@
+"""
+MIT License
+
+Copyright (c) 2018 Claude SIMON (https://q37.info/s/rmnmqd49)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import os, sys
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append("../../atlastk")
+
+import atlastk
+
+BODY = """
+<span style="margin: 5px; color:white; background-color: grey; padding: 5px;">Feet to Meters</span>
+<fieldset style="display: grid;gap: 10px; background-color: lightgrey;">
+  <input id="FeetEntry" xdh:onevent="Calculate" style="grid-column: 2;" size="7"/>
+  <span style="margin: auto 0">feet</span>
+  <span>is equivalent to</span>
+  <output id="Meters"></output>
+  <span>meters</span>
+  <button xdh:onevent="Calculate" style="grid-column: 3;">Calculate</span>
+</fieldset>
+"""
+
+def acConnect(dom):
+  dom.inner("", BODY)
+  dom.focus("FeetEntry")
+
+
+def acCalculate(dom):
+  try:
+    value = float(dom.getValue("FeetEntry"))
+    dom.setValue("Meters", int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+  except ValueError:
+    pass  
+
+
+CALLBACKS = {
+  "": acConnect,
+  "Calculate": acCalculate
+}
+
+atlastk.launch(CALLBACKS, headContent="<title>Feet to Meters</title>")
